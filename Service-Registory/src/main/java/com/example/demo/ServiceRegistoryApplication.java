195bbd274267cb908bc84c1bb94bdca10ad804a0
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-01 13:00:52 +0530

    @EnableEurekaServer it tell to the IOC the application is for to register Services which are working in

--- Service-Registory/src/main/java/com/example/demo/ServiceRegistoryApplication.java
+++ Service-Registory/src/main/java/com/example/demo/ServiceRegistoryApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistoryApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,8 @@ public class ServiceRegistoryApplication {
 	}
 
 }
+
+// @EnableEurekaServer
+// it tell to the IOC the application is for to register Services which are working in
+
+
