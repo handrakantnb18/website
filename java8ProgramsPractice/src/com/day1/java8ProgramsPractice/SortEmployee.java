1af6e9601b6adcfc9cb9ef410078c8a7e946d210
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-02 10:08:56 +0530

    SortEmployee as per salary

---
+++ java8ProgramsPractice/src/com/day1/java8ProgramsPractice/SortEmployee.java
@@ -0,0 +1,21 @@
+package com.day1.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortEmployee {
+
+	public static void main(String[] args) {
+		
+		List<Student> list = Arrays.asList(
+				new Student(101, "Chandrakant", 5500.00),
+				new Student(102, "Priyanka", 44000.00),
+				new Student(103, "Shivraj", 33000.00));
+		
+		list.stream()
+		.sorted(Comparator.comparingDouble(e -> e.getSalary()))
+		.forEach(System.out::println);
+		
+	}
+}
