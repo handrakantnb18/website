687ba0486817897ddcdfe5b457722548062a3ace
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-02 10:07:16 +0530

    Find Maximum and Minimum

---
+++ java8ProgramsPractice/src/com/day1/java8ProgramsPractice/MinMax.java
@@ -0,0 +1,29 @@
+package com.day1.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MinMax {
+
+	// Find Maximum and Minimum
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(
+				10,22,25,35,7,8,95,55,70);
+		
+		Integer max =
+			list.stream()
+			.max(Integer::compare)
+			.get();
+			
+			System.out.println("Max Number : "+max);
+		
+		Integer min =
+			list.stream()
+			.min(Integer::compare)
+			.get();
+			
+			System.out.println("Min Number : "+min);
+			
+	}
+}
