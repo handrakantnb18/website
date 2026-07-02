f27e0c36be3323919dc2336bda3f5339e1e79ef5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-02 10:07:45 +0530

    Remove Duplicate Elements

---
+++ java8ProgramsPractice/src/com/day1/java8ProgramsPractice/SortIdStudent.java
@@ -0,0 +1,26 @@
+package com.day1.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class SortIdStudent {
+
+	public static void main(String[] args) {
+		
+		// Convert List into Map
+		List<Student> list = Arrays.asList(
+				new Student(1, "Rahul", 7898788.00),
+				new Student(2, "OM", 456325.00),
+				new Student(3, "Omkar", 258789.00)
+				);
+		
+		Map<Integer, Student> map =
+		list.stream()
+		.collect(Collectors.toMap(Student::getId, s->s));
+		
+		System.out.println(map);
+	}
+}
