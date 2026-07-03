6f48c9238be5fef97d6a3b999a8f7d612051c94f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-03 20:32:08 +0530

    user POJO class created

---
+++ java8ProgramsPractice/src/com/day3/java8ProgramsPractice/User.java
@@ -0,0 +1,59 @@
+package com.day3.java8ProgramsPractice;
+
+public class User {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String mobileNo;
+	
+	private String city;
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", mobileNo=" + mobileNo + ", city=" + city + "]";
+	}
+
+	public User(Integer id, String name, String mobileNo, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mobileNo = mobileNo;
+		this.city = city;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+		
+}
