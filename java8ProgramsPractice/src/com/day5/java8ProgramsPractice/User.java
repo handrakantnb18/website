090278962a5f3274a3306de3a863c0b5c42f511c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-05 14:50:35 +0530

    getter and setter method created as per User class

--- java8ProgramsPractice/src/com/day5/java8ProgramsPractice/User.java
+++ java8ProgramsPractice/src/com/day5/java8ProgramsPractice/User.java
@@ -22,6 +22,38 @@ public class User {
 		this.address = address;
 		this.mobileNo = mobileNo;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
 	
 	
 }
