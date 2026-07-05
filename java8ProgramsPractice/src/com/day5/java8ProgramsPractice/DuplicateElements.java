1e0475e751c28ce3ee6ff95fd2bb6666e3200ab3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-05 14:00:47 +0530

    Duplicate Elements in List of Arrays

---
+++ java8ProgramsPractice/src/com/day5/java8ProgramsPractice/DuplicateElements.java
@@ -0,0 +1,44 @@
+package com.day5.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DuplicateElements {
+
+	public static void main(String[] args) {
+		
+		System.out.println("First List : ");
+		List<Integer> list1 = Arrays.asList(
+				1,5,9,7,8,1,5,6,3,4,8,9,10);
+		
+		Set<Integer> set1 = new HashSet<Integer>(); 
+		
+		list1.stream()
+		.filter(d1 -> !set1.add(d1))
+		.forEach(System.out::println);
+		
+		System.out.println("Second List : ");
+		List<Integer> list2 = Arrays.asList(
+				7,89,5,2,3,5,4,1,5,89,3,7,1);
+		
+		Set<Integer> set2 = new HashSet<Integer>();
+		
+		list2.stream()
+		.filter(d2 -> !set2.add(d2))
+		.forEach(System.out::println);
+		
+		System.out.println("Third List : ");
+		List<Integer> list3 = Arrays.asList(
+				8,3,5,4,8,3,1,9,6,5,7,1,9);
+		
+		Set<Integer> set3 = new HashSet<Integer>();
+		
+		list3.stream()
+		.filter(d3 -> !set3.add(d3))
+		.forEach(System.out::println);
+		
+		
+	}
+}
