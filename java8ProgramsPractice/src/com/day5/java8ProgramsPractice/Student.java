646a0e4fc77cbfe7e656ddde12bd226e754b4288
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-05 14:38:06 +0530

    Student class private variables and getter and setter method created

---
+++ java8ProgramsPractice/src/com/day5/java8ProgramsPractice/Student.java
@@ -0,0 +1,47 @@
+package com.day5.java8ProgramsPractice;
+
+public class Student {
+
+private Integer id;
+	
+	private String name;
+	
+	private Double salary;
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+	public Student(Integer id, String name, Double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+}
