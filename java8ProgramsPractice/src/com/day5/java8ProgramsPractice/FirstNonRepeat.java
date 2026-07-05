dd6b1b3f106efb6a687f39af7e52e84e1a4ae715
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-05 14:24:42 +0530

    First Non Repeat word find

---
+++ java8ProgramsPractice/src/com/day5/java8ProgramsPractice/FirstNonRepeat.java
@@ -0,0 +1,31 @@
+package com.day5.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FirstNonRepeat {
+
+	public static void main(String[] args) {
+		
+		
+			String str = "iojijsiojvoi";
+		
+		String string = 
+				Arrays.stream(str.split(""))
+				.collect(Collectors.groupingBy(
+						Function.identity(),
+						LinkedHashMap::new,
+						Collectors.counting()))
+				.entrySet()
+				.stream()
+				.filter(n -> n.getValue() == 1)
+				.findFirst()
+				.get()
+				.getKey();
+			
+			System.out.println(string);
+		
+	}
+}
