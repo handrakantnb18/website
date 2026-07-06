9766355cd540531f67b2bf760e892275c2f7337b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-06 20:33:05 +0530

    priavate variable in product class

---
+++ java8ProgramsPractice/src/com/day6/java8ProgramsPractice/Product.java
@@ -0,0 +1,14 @@
+package com.day6.java8ProgramsPractice;
+
+public class Product {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String address;
+	
+	private String mobileNo;
+	
+	
+}
