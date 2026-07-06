6d992f2a5c1d18a3d1ef34cc7086f8acbd1b0a92
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-06 20:37:21 +0530

    setter and getter methods and toString methods

--- java8ProgramsPractice/src/com/day6/java8ProgramsPractice/Employee.java
+++ java8ProgramsPractice/src/com/day6/java8ProgramsPractice/Employee.java
@@ -9,6 +9,56 @@ public class Employee {
 	private String Dept;
 	
 	private String experience;
+
+	public Employee(Integer id, String empName, String dept, String experience) {
+		super();
+		this.id = id;
+		this.empName = empName;
+		Dept = dept;
+		this.experience = experience;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", empName=" + empName + ", Dept=" + Dept + ", experience=" + experience + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getDept() {
+		return Dept;
+	}
+
+	public void setDept(String dept) {
+		Dept = dept;
+	}
+
+	public String getExperience() {
+		return experience;
+	}
+
+	public void setExperience(String experience) {
+		this.experience = experience;
+	}
 	
 	
 }
