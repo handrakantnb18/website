1b41dc617f312e7f713df739bf61ab03b53035b1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-06 20:36:05 +0530

    constructor and private variables are added

---
+++ java8ProgramsPractice/src/com/day6/java8ProgramsPractice/Employee.java
@@ -0,0 +1,14 @@
+package com.day6.java8ProgramsPractice;
+
+public class Employee {
+
+	private Integer id;
+	
+	private String empName;
+	
+	private String Dept;
+	
+	private String experience;
+	
+	
+}
