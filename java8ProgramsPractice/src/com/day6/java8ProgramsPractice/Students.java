6e450d77e681786dabc295f3d994e1efa1b42054
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-06 20:30:36 +0530

    getter and setter method and constructor added

--- java8ProgramsPractice/src/com/day6/java8ProgramsPractice/Students.java
+++ java8ProgramsPractice/src/com/day6/java8ProgramsPractice/Students.java
@@ -7,6 +7,47 @@ public class Students {
 	private String name;
 	
 	private String city;
+
+	@Override
+	public String toString() {
+		return "Students [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+
+	public Students(Integer id, String name, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+	}
+
+	public Students() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
 	
 	
 }
