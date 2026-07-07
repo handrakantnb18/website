cc7e2f795ab2efb95036cf0efc7ec4f59a1387f5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-07 21:36:26 +0530

    UserNotFoundException extends RuntimeException

---
+++ Exceptions/src/com/allcustome/exceptions/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.allcustome.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+
+	public UserNotFoundException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+}
