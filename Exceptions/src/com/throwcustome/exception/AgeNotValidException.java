ecd6de744763e4db1583b94b6e6cdfec7fd4f301
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-07 21:57:13 +0530

    +public class AgeNotValidException extends RuntimeException {

--- Exceptions/src/com/throwcustome/exception/AgeNotValidException.java
+++ Exceptions/src/com/throwcustome/exception/AgeNotValidException.java
@@ -1,6 +1,10 @@
 package com.throwcustome.exception;
 
-public class AgeNotValidException {
+public class AgeNotValidException extends RuntimeException {
 
+	public AgeNotValidException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
 	
 }
