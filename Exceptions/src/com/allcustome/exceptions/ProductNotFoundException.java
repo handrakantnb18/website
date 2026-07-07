5a6e04dc4cd2e0bf14c23a63cd03dd25537c5b1b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-07 21:35:53 +0530

    ProductNotFoundException extends RuntimeException

---
+++ Exceptions/src/com/allcustome/exceptions/ProductNotFoundException.java
@@ -0,0 +1,10 @@
+package com.allcustome.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+	
+	public ProductNotFoundException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+
+}
