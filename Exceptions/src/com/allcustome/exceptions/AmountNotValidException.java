8d7c4dbfc255afe49d7821896f3fe9584645a4fc
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-07 21:33:55 +0530

    AmountNotValidException RuntimeException

---
+++ Exceptions/src/com/allcustome/exceptions/AmountNotValidException.java
@@ -0,0 +1,9 @@
+package com.allcustome.exceptions;
+
+public class AmountNotValidException extends RuntimeException {
+
+	public AmountNotValidException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+}
