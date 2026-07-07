63e44bcc626a205ab5310a1033668e49ede86291
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-07 21:35:19 +0530

    InsufficentBalanceException RuntimeException

---
+++ Exceptions/src/com/allcustome/exceptions/InsufficentBalanceException.java
@@ -0,0 +1,9 @@
+package com.allcustome.exceptions;
+
+public class InsufficentBalanceException extends RuntimeException {
+
+	public InsufficentBalanceException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+}
