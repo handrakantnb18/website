7dee4ed6c5a63c54d62b18697df93eb04815b9b8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-07 21:39:15 +0530

    InsufficentBalance balanceCheck() throw new InsufficentBalanceException("Insufficent Balance");

---
+++ Exceptions/src/com/throwcustome/exception/InsufficentBalance.java
@@ -0,0 +1,31 @@
+package com.throwcustome.exception;
+
+import com.allcustome.exceptions.InsufficentBalanceException;
+import java.util.Scanner;
+
+public class InsufficentBalance {
+
+	void balanceCheck()
+	{
+		Scanner scr = new Scanner(System.in);
+		System.out.println("Enter amount");
+		
+		int balance = scr.nextInt();
+		
+		if(balance < 0)
+		{
+			throw new InsufficentBalanceException("Insufficent Balance");
+		}
+		else
+		{
+			System.out.println("Balance is available");
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		InsufficentBalance ifb = new InsufficentBalance();
+		
+		ifb.balanceCheck();
+	}
+}
