91e2e23ea42a3febf4e7999094ee4aec5366ec70
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-07 21:34:24 +0530

    EmployeeNotFoundException RuntimeException

---
+++ Exceptions/src/com/allcustome/exceptions/EmployeeNotFoundException.java
@@ -0,0 +1,10 @@
+package com.allcustome.exceptions;
+
+public class EmployeeNotFoundException extends RuntimeException {
+	
+	public EmployeeNotFoundException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+
+}
