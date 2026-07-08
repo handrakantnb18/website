312f1727267d8855d9c7b03d376b24f6d7053eec
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-08 20:26:39 +0530

    getter and setter method and constructor

--- java8ProgramsPractice/src/com/day7/java8ProgramsPractice/Employee.java
+++ java8ProgramsPractice/src/com/day7/java8ProgramsPractice/Employee.java
@@ -17,6 +17,60 @@ public class Employee {
 		return "Employee [id=" + id + ", name=" + name + ", address=" + address + ", mobileNo=" + mobileNo + ", salary="
 				+ salary + "]";
 	}
+
+	public Employee(Integer id, String name, String address, String mobileNo, Integer salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.mobileNo = mobileNo;
+		this.salary = salary;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public Integer getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
 	
 	
 }
