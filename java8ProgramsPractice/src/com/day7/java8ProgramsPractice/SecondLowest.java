e89b8c73822d49a71a4d3263544f41b32b279b82
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-08 20:03:26 +0530

    Second Lowest Number in the list

---
+++ java8ProgramsPractice/src/com/day7/java8ProgramsPractice/SecondLowest.java
@@ -0,0 +1,24 @@
+package com.day7.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SecondLowest {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(78,9,87,65,35,
+				12,35,48,55,78,98,56,32);
+		
+		Integer lowest = 
+				list.stream()
+				.distinct()
+				.sorted()
+				.skip(1)
+				.findFirst()
+				.get();
+		
+		System.out.println(lowest);
+		
+	}
+}
