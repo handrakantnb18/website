b6e6feb61a316b1e91632dffb377dcd030ad720c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-09 22:57:18 +0530

    FirstNonRepeat in the string

---
+++ java8ProgramsPractice/src/com/day8/java8ProgramsPractice/FirstNonRepeat.java
@@ -0,0 +1,31 @@
+package com.day8.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FirstNonRepeat {
+
+	public static void main(String[] args) {
+		
+		
+			String str = "priyanka";
+		
+		String string = 
+				Arrays.stream(str.split(""))
+				.collect(Collectors.groupingBy(
+						Function.identity(),
+						LinkedHashMap::new,
+						Collectors.counting()))
+				.entrySet()
+				.stream()
+				.filter(n -> n.getValue() == 1)
+				.findFirst()
+				.get()
+				.getKey();
+			
+			System.out.println(string);
+		
+	}
+}
