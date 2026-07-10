ec2402581fec8aba9e8f84c7d811a2d2e1309233
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-10 22:42:19 +0530

    Odd Numbers in the list

---
+++ java8ProgramsPractice/src/com/day9/java8ProgramsPractice/OddNumbersList.java
@@ -0,0 +1,19 @@
+package com.day9.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class OddNumbersList {
+
+	public static void main(String[] args) {
+		
+		// even numbers
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10
+				,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25);
+		
+		list.stream()
+		.filter(o -> o % 2 != 0)
+		.forEach(System.out::println);
+
+	}
+}
