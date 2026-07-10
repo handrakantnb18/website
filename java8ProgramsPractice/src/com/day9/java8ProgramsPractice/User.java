48402984983d5642aecf9950bb43047262cf3b5b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-10 22:52:41 +0530

    getter and setter method and constructor method added

--- java8ProgramsPractice/src/com/day9/java8ProgramsPractice/User.java
+++ java8ProgramsPractice/src/com/day9/java8ProgramsPractice/User.java
@@ -15,5 +15,51 @@ private Integer id;
 		return "User [id=" + id + ", name=" + name + ", mobileNo=" + mobileNo + ", city=" + city + "]";
 	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public User(Integer id, String name, String mobileNo, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mobileNo = mobileNo;
+		this.city = city;
+	}
+
 	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
 }
