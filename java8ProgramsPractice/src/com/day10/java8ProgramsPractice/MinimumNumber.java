0596a08ee27fb32a11a1f33a22f72d121f57a77d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-11 22:21:57 +0530

    Minimum Number System.out.println("Max Number : "+max);

---
+++ java8ProgramsPractice/src/com/day10/java8ProgramsPractice/MinimumNumber.java
@@ -0,0 +1,21 @@
+package com.day10.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MinimumNumber {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(
+				10,22,25,35,7,8,95,55,70);
+		
+		Integer max =
+			list.stream()
+			.max(Integer::compare)
+			.get();
+			
+			System.out.println("Max Number : "+max);
+		
+	}
+}
