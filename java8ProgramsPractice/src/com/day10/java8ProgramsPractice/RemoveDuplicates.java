6fde114864ed7b363c45b5efd17401d01fad71d4
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-11 22:25:57 +0530

    Remove Duplicate Elements RemoveDuplicates

---
+++ java8ProgramsPractice/src/com/day10/java8ProgramsPractice/RemoveDuplicates.java
@@ -0,0 +1,20 @@
+package com.day10.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RemoveDuplicates {
+
+	public static void main(String[] args) {
+		
+		// Remove Duplicate Elements
+		
+				List<Integer> list = Arrays.asList(
+						1,5,6,3,5,78,9,1,6);
+				
+				list.stream()
+				.distinct()
+				.forEach(System.out::println);
+				
+	}
+}
