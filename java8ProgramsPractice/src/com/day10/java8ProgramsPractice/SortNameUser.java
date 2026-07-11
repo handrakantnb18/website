2bf6dbb628b1426237191a2dad6a394b61d0253c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-11 22:39:25 +0530

    Sort NameUser .filter(c -> c.getCity().equals("Pune"))

---
+++ java8ProgramsPractice/src/com/day10/java8ProgramsPractice/SortNameUser.java
@@ -0,0 +1,31 @@
+package com.day10.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.day2.java8ProgramsPractice.Employee;
+
+public class SortNameUser {
+
+	public static void main(String[] args) {
+		
+		// Convert List into Map
+		List<User> list = Arrays.asList(
+				new User(1, "Rahul", "Pune", "9876543210"),
+				new User(2, "OM", "mumbai", "987652523210"),
+				new User(3, "Omkar", "Pune", "9876883210"),
+				new User(4, "Ram", "mumbai", "94568583210")
+				);
+		List<User> city = 
+				list.stream()
+				.filter(c -> c.getCity().equals("Pune"))
+				.collect(Collectors.toList());
+		
+		
+		
+		System.out.println(city);
+
+	}
+}
