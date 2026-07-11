1e4e17270dd6743734f61d61f3d8f5577d4cce3b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-11 22:55:25 +0530

    toString method and getter and setter method

--- java8ProgramsPractice/src/com/day10/java8ProgramsPractice/Student.java
+++ java8ProgramsPractice/src/com/day10/java8ProgramsPractice/Student.java
@@ -22,6 +22,44 @@ public class Student {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", city=" + city + ", mobileNo=" + mobileNo + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	
 	
 	
 }
