b9689bfd7addeb32e725e68db828dcf934d6a5b9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-11 22:49:30 +0530

    Student class private variables and constructor na ddefault

---
+++ java8ProgramsPractice/src/com/day10/java8ProgramsPractice/Student.java
@@ -0,0 +1,27 @@
+package com.day10.java8ProgramsPractice;
+
+public class Student {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String city;
+	
+	private String mobileNo;
+
+	public Student(Integer id, String name, String city, String mobileNo) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+		this.mobileNo = mobileNo;
+	}
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
