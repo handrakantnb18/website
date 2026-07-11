95d74bdc5b9b111950507ea572487c89be14a52f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-11 22:15:13 +0530

    constructor and getter and setter emthod added

--- java8ProgramsPractice/src/com/day10/java8ProgramsPractice/Employee.java
+++ java8ProgramsPractice/src/com/day10/java8ProgramsPractice/Employee.java
@@ -14,6 +14,52 @@ public class Employee {
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", city=" + city + "]";
 	}
+
+	public Employee(Integer id, String name, String salary, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.city = city;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSalary() {
+		return salary;
+	}
+
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
 	
 	
 }
