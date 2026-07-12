1859d3936cde0eebdea450a7510b0a6ae73398ba
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-12 11:57:13 +0530

    AIService	private final ChatClient chatClient;

--- AI-ChatBoot/src/main/java/com/example/demo/aiagent/AIService.java
+++ AI-ChatBoot/src/main/java/com/example/demo/aiagent/AIService.java
@@ -1,7 +1,26 @@
 package com.example.demo.aiagent;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AIService {
 
 	
+	private final ChatClient chatClient;
+	
+	public AIService(ChatClient.Builder builder)
+	{
+		this.chatClient = builder.build();
+	}
+	
+	public String askAI(String question)
+	{
+		
+		return chatClient
+				.prompt(question)
+				.call()
+				.content();
+		
+	}
 	
 }
