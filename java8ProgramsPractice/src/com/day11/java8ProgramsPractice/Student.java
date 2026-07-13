2b0a1f716dab1c5352d0b4c77bb9860faee5dc66
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-13 23:35:56 +0530

    Student class create cariables

---
+++ java8ProgramsPractice/src/com/day11/java8ProgramsPractice/Student.java
@@ -0,0 +1,19 @@
+package com.day11.java8ProgramsPractice;
+
+public class Student {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String collage;
+	
+	private String address;
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", collage=" + collage + ", address=" + address + "]";
+	}
+	
+	
+}
