f906ca534e87675ea548714a98810e2a29b6a2d2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-13 23:35:26 +0530

    Employee class create cariables

---
+++ java8ProgramsPractice/src/com/day11/java8ProgramsPractice/Employee.java
@@ -0,0 +1,22 @@
+package com.day11.java8ProgramsPractice;
+
+public class Employee {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String address;
+	
+	private String salary;
+	
+	private String dept;
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", address=" + address + ", salary=" + salary + ", dept="
+				+ dept + "]";
+	}
+	
+	
+}
