8f8372b3da0d96a2ef5d62c40d5104ff635dd9ee
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-13 23:34:42 +0530

    Book class create cariables

---
+++ java8ProgramsPractice/src/com/day11/java8ProgramsPractice/Book.java
@@ -0,0 +1,19 @@
+package com.day11.java8ProgramsPractice;
+
+public class Book {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String dept;
+	
+	private String PRNnumber;
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", dept=" + dept + ", PRNnumber=" + PRNnumber + "]";
+	}
+	
+	
+}
