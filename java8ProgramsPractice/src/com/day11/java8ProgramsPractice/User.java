67ab7feda7b515d51737a23ba97dd2a61009304f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-13 23:36:19 +0530

    User class create cariables

---
+++ java8ProgramsPractice/src/com/day11/java8ProgramsPractice/User.java
@@ -0,0 +1,21 @@
+package com.day11.java8ProgramsPractice;
+
+public class User {
+
+	
+	private Integer id;
+	
+	private String name;
+	
+	private String city;
+	
+	private String mobileNo;
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", city=" + city + ", mobileNo=" + mobileNo + "]";
+	}
+	
+	
+	
+}
