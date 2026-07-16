350afbee762f818d8b1ee561a15e0ecb17cd96c6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-16 15:24:44 +0530

    public Integer getId() {

--- java8ProgramsPractice/src/com/day11/java8ProgramsPractice/Student.java
+++ java8ProgramsPractice/src/com/day11/java8ProgramsPractice/Student.java
@@ -14,6 +14,38 @@ public class Student {
 	public String toString() {
 		return "Student [id=" + id + ", name=" + name + ", collage=" + collage + ", address=" + address + "]";
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCollage() {
+		return collage;
+	}
+
+	public void setCollage(String collage) {
+		this.collage = collage;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	
 	
 }
