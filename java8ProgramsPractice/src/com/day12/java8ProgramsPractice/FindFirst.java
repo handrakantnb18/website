bdc35eeeee8453434781ff8824618db641ea8da7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-17 16:51:24 +0530

    Find First Element

---
+++ java8ProgramsPractice/src/com/day12/java8ProgramsPractice/FindFirst.java
@@ -0,0 +1,19 @@
+package com.day12.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FindFirst {
+
+	// Find First Element
+	public static void main(String[] args) {
+		String str = "kjkifhedhfiduah";
+		
+//		List<Integer> list = Arrays.asList(
+//				78,95,66,45,32,18,19,20,25,67,80);
+		
+//		str.
+//		.findFirst()
+//		.isPresent(System.out::println);
+	}
+}
