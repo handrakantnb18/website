e700516f87a54f71bc0c9ddde313472c5a2b4ed3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-18 22:35:44 +0530

    Sort Decending list

---
+++ java8ProgramsPractice/src/com/day13/java8ProgramsPractice/SortDecending.java
@@ -0,0 +1,20 @@
+package com.day13.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SortDecending {
+
+	public static void main(String[] args) {
+		
+		List<Integer> num = Arrays.asList(
+				25,64,29,37,88,92,13,46,5);
+		
+		num.stream()
+		.sorted(Collections.reverseOrder())
+		.forEach(System.out::println);
+	
+		
+	}
+}
