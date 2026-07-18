b511d429dcad8a0c2aad4fb95522d25050390a35
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-18 18:41:35 +0530

    Student c;as  private variables and toString method added

---
+++ java8ProgramsPractice/src/com/day13/java8ProgramsPractice/Student.java
@@ -0,0 +1,19 @@
+package com.day13.java8ProgramsPractice;
+
+public class Student {
+
+	private Integer sid;
+	
+	private String sname;
+	
+	private String city;
+	
+	private String college;
+
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", city=" + city + ", college=" + college + "]";
+	}
+	
+	
+}
