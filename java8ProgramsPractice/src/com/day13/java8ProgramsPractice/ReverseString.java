120057bcd89d1330da6133a91bbbe04dd5c9da61
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-18 22:22:08 +0530

    Reverse String in java8

--- java8ProgramsPractice/src/com/day13/java8ProgramsPractice/ReverseString.java
+++ java8ProgramsPractice/src/com/day13/java8ProgramsPractice/ReverseString.java
@@ -13,5 +13,14 @@ public class ReverseString {
 				.reduce("", (a, b) -> b+a);
 		
 		System.out.println(rev);
+		
+		String str1 = "priyanka";
+		
+		String rev1 = 
+				Arrays.stream(str1.split(""))
+				.reduce("", (x,y) -> y+x);
+		
+		System.out.println(rev1);
+		
 	}
 }
