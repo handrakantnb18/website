af2e9567822a4c01de618048236672014cfa520c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-18 22:32:29 +0530

    Find Second Lowest in the list

---
+++ java8ProgramsPractice/src/com/day13/java8ProgramsPractice/FindSecondLowest.java
@@ -0,0 +1,24 @@
+package com.day13.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FindSecondLowest {
+
+	public static void main(String[] args) {
+			
+			List<Integer> list = Arrays.asList(
+					78,98,62,45,35,11,25,38,72);
+			
+			Integer sec = 
+					list.stream()
+					.distinct()
+					.sorted()
+					.skip(1)
+					.findFirst()
+					.get(); 
+			
+			System.out.println(sec);
+		
+	}
+}
