3aaaf7dee5c645f7ac4249962a5cf4c3e482515a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-18 20:39:03 +0530

    getter setter method and constructor are added

--- java8ProgramsPractice/src/com/day13/java8ProgramsPractice/Bank.java
+++ java8ProgramsPractice/src/com/day13/java8ProgramsPractice/Bank.java
@@ -17,6 +17,55 @@ public class Bank {
 		return "Bank [accountNumber=" + accountNumber + ", bankName=" + bankName + ", bankHolderName=" + bankHolderName
 				+ ", branch=" + branch + ", balance=" + balance + "]";
 	}
+
+	public Bank(Integer accountNumber, String bankName, String bankHolderName, String branch, Double balance) {
+		super();
+		this.accountNumber = accountNumber;
+		this.bankName = bankName;
+		this.bankHolderName = bankHolderName;
+		this.branch = branch;
+		this.balance = balance;
+	}
+
+	public Integer getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(Integer accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+
+	public String getBankHolderName() {
+		return bankHolderName;
+	}
+
+	public void setBankHolderName(String bankHolderName) {
+		this.bankHolderName = bankHolderName;
+	}
+
+	public String getBranch() {
+		return branch;
+	}
+
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
 	
 	
 }
