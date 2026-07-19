99d6783e383b62e45875073d0e5dd358aaef8452
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-19 22:24:46 +0530

    using map key and value pair to get data id and name

---
+++ java8ProgramsPractice/src/com/day14/java8ProgramsPractice/AdminIdName.java
@@ -0,0 +1,36 @@
+package com.day14.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class AdminIdName {
+
+	public static void main(String[] args) {
+		
+		List<Admin> list = Arrays.asList(
+				new Admin(111, "Ram", "ram@gmail.com", "IT"),
+				new Admin(111, "Ram", "ram@gmail.com", "IT"),
+		        new Admin(112, "Shyam", "shyam@gmail.com", "HR"),
+		        new Admin(113, "Rahul", "rahul@gmail.com", "Finance"),
+		        new Admin(114, "Amit", "amit@gmail.com", "Admin"),
+		        new Admin(115, "Sachin", "sachin@gmail.com", "Development"),
+		        new Admin(116, "Priya", "priya@gmail.com", "Testing"),
+		        new Admin(117, "Sneha", "sneha@gmail.com", "Support"),
+		        new Admin(118, "Kiran", "kiran@gmail.com", "Security"),
+		        new Admin(119, "Neha", "neha@gmail.com", "Marketing"),
+		        new Admin(120, "Vikas", "vikas@gmail.com", "HR")
+				
+				);
+		
+		Map<Integer, String> map=
+				list.stream()
+				.collect(Collectors.toMap(
+						Admin::getId, Admin::getName, (oldValue, newValue) 
+						-> oldValue));
+		
+		map.forEach((key, value) -> System.out.println(key + " : "+value));
+		
+	}
+}
