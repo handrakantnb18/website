b010ddba16998b701132656aafafd1a6d50485bb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-19 22:03:48 +0530

    getter and setter method constructor added

--- java8ProgramsPractice/src/com/day14/java8ProgramsPractice/Admin.java
+++ java8ProgramsPractice/src/com/day14/java8ProgramsPractice/Admin.java
@@ -14,6 +14,46 @@ public class Admin {
 	public String toString() {
 		return "Admin [id=" + id + ", name=" + name + ", email=" + email + ", dept=" + dept + "]";
 	}
+
+	public Admin(Integer id, String name, String email, String dept) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.dept = dept;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
 	
 	
 }
