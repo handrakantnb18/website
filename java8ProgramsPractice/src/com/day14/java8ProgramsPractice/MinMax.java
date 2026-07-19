c610546531a2a9754c636a5a3d657bf4a6d10bbc
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-19 22:30:20 +0530

    Minimum and Maximum number found

---
+++ java8ProgramsPractice/src/com/day14/java8ProgramsPractice/MinMax.java
@@ -0,0 +1,29 @@
+package com.day14.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MinMax {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(
+				10,22,25,35,7,8,95,55,70);
+		
+		Integer max =
+			list.stream()
+			.max(Integer::compare)
+			.get();
+			
+			System.out.println("Max Number : "+max);
+		
+		Integer min =
+			list.stream()
+			.min(Integer::compare)
+			.get();
+			
+			System.out.println("Min Number : "+min);
+			
+
+	}
+}
