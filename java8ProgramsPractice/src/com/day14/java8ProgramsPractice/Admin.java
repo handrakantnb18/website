d368e5ecfaee27d9a63b364c6001ca1346cce75f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-19 22:02:39 +0530

    Admin class private variables and toString method

---
+++ java8ProgramsPractice/src/com/day14/java8ProgramsPractice/Admin.java
@@ -0,0 +1,19 @@
+package com.day14.java8ProgramsPractice;
+
+public class Admin {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String dept;
+
+	@Override
+	public String toString() {
+		return "Admin [id=" + id + ", name=" + name + ", email=" + email + ", dept=" + dept + "]";
+	}
+	
+	
+}
