58464d4742fb930cae5c95b097d1bd7549e0ebc4
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-19 14:27:52 +0530

    User class private variables and toString method

---
+++ java8ProgramsPractice/src/com/day14/java8ProgramsPractice/User.java
@@ -0,0 +1,22 @@
+package com.day14.java8ProgramsPractice;
+
+public class User {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String address;
+	
+	private String email;
+	
+	private String mobileNo;
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", address=" + address + ", email=" + email + ", mobileNo="
+				+ mobileNo + "]";
+	}
+	
+	
+}
