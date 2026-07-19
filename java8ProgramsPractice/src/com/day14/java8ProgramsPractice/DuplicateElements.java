d38d730b5d7b990ed274966563dd0b990f806e04
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-19 22:29:00 +0530

    Duplicate Elements	.filter(n -> !set.add(n))

---
+++ java8ProgramsPractice/src/com/day14/java8ProgramsPractice/DuplicateElements.java
@@ -0,0 +1,22 @@
+package com.day14.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DuplicateElements {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(
+				10,15,23,15,78,98,78,22,35,22);
+		
+		Set<Integer> set = new HashSet<Integer>();
+		
+		list.stream()
+		.filter(n -> !set.add(n))
+		.forEach(System.out::println);
+	
+	}
+}
