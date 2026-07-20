bf45681b083fd47ced760702afac9694032c328b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-20 18:55:13 +0530

    Reverse String multiple example in the String

--- java8ProgramsPractice/src/com/day15/java8ProgramsPractice/ReverseString.java
+++ java8ProgramsPractice/src/com/day15/java8ProgramsPractice/ReverseString.java
@@ -13,5 +13,35 @@ public class ReverseString {
 				.reduce("", (a,b) -> b+a);
 		
 		System.out.println(rev);
+		
+		System.out.println("============================");
+		String str1 = "chandrakant Nana Bhosale";
+		
+		String rev1 =
+				Arrays.stream(str1.split(""))
+				.reduce("", (x,y) -> y+x);
+		
+		System.out.println(rev1);
+		
+		System.out.println("===========================");
+		
+		String str2 = "how are you";
+		
+		String rev2 =
+				Arrays.stream(str2.split(""))
+				.reduce("", (p,q) -> q+p);
+		
+		System.out.println(rev2);
+		
+		
+		System.out.println("============================");
+		
+		String str3 = "welcome to software development";
+		
+		String rev4 = 
+				Arrays.stream(str3.split(""))
+				.reduce("", (d,e) -> e+d);
+		
+		System.out.println(rev4);
 	}
 }
