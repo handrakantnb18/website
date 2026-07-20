1f2d2b2dbb6b3ecb149ff166ead82c281de422d0
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-20 20:59:16 +0530

    getter and setter method and constructor

--- java8ProgramsPractice/src/com/day15/java8ProgramsPractice/Student.java
+++ java8ProgramsPractice/src/com/day15/java8ProgramsPractice/Student.java
@@ -17,6 +17,60 @@ public class Student {
 		return "Student [id=" + id + ", name=" + name + ", collage=" + collage + ", email=" + email + ", mobileNo="
 				+ mobileNo + "]";
 	}
+
+	public Student(Integer id, String name, String collage, String email, String mobileNo) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.collage = collage;
+		this.email = email;
+		this.mobileNo = mobileNo;
+	}
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCollage() {
+		return collage;
+	}
+
+	public void setCollage(String collage) {
+		this.collage = collage;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
 	
 	
 }
