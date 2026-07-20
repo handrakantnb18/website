88578fe12d0972bc6c9f1fa24b99b89838007f3a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-20 12:27:29 +0530

    connecting API resource

---
+++ ProductAPI/src/main/java/com/example/demo/controller/ContactController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ContactController {
+
+//	 public String sendContact(ContactForm form)
+//	 {
+//		 emailService.sendContactMail(form);
+//		 
+//		 return "Success";
+//	 }
+	
+}
