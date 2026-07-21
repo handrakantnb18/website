66e4b598b3695ea33259f1d3d752710acb7495f2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-21 11:41:12 +0530

    Reverse String using map and StringBuilder

--- java8ProgramsPractice/src/com/day16/java8ProgramsPractice/ReverseString.java
+++ java8ProgramsPractice/src/com/day16/java8ProgramsPractice/ReverseString.java
@@ -8,13 +8,52 @@ public class ReverseString {
 
 	public static void main(String[] args) {
 		
-		String str = "chandrakant bhosale";
+		String str1 = "chandrakant bhosale";
 		
 		String rev =
-				Arrays.stream(str.split(""))
+				Arrays.stream(str1.split(""))
 				.reduce("", (a,b) -> b+a);
 		
 		System.out.println(rev);
+		
+		System.out.println("===========================");
+		
+		String str2 = "who are you bro";
+
+		String result = 
+				Arrays.stream(str2.split(" "))
+				.map(word -> new StringBuilder(word)
+						.reverse()
+						.toString())
+				.collect(Collectors.joining(" "));
+
+		System.out.println(result);
+
+		System.out.println("=========================");
+
+		String str3 = "Reverse String";
+		
+		String res =
+				Arrays.stream(str3.split(" "))
+				.map(x -> new StringBuilder(x)
+						.reverse()
+						.toString())
+				.collect(Collectors.joining(" "));
+		
+		System.out.println(res);
+		
+		System.out.println("=======================");
+		
+		String str4 = "my name is chandrakant";
+		
+		String rv =
+				Arrays.stream(str4.split(" "))
+				.map(y -> new StringBuilder(y)
+						.reverse()
+						.toString())
+				.collect(Collectors.joining(" "));
+		
+		System.out.println(rv);
 		
 	}
 }
