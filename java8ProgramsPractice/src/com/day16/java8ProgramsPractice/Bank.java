732a92672d23d5ff31d95971922da053a8b12bc5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-21 11:52:55 +0530

    Bank class constructor and getter and setteer method used

--- java8ProgramsPractice/src/com/day16/java8ProgramsPractice/Bank.java
+++ java8ProgramsPractice/src/com/day16/java8ProgramsPractice/Bank.java
@@ -17,6 +17,60 @@ public class Bank {
 		return "Bank [id=" + id + ", name=" + name + ", accountNo=" + accountNo + ", email=" + email + ", mobileNo="
 				+ mobileNo + "]";
 	}
+
+	public Bank(Integer id, String name, Double accountNo, String email, Long mobileNo) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.accountNo = accountNo;
+		this.email = email;
+		this.mobileNo = mobileNo;
+	}
+
+	public Bank() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(Double accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(Long mobileNo) {
+		this.mobileNo = mobileNo;
+	}
 	
 	
 }
