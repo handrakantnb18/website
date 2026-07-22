789c19d70de08193947ba0b04b6e886c713e39b8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-22 20:40:59 +0530

    getter and setter method and constructor

--- java8ProgramsPractice/src/com/day17/java8ProgramsPractice/Student.java
+++ java8ProgramsPractice/src/com/day17/java8ProgramsPractice/Student.java
@@ -14,6 +14,46 @@ public class Student {
 	public String toString() {
 		return "Student [id=" + id + ", name=" + name + ", address=" + address + ", mobileNo=" + mobileNo + "]";
 	}
+
+	public Student(Integer id, String name, String address, Long mobileNo) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.mobileNo = mobileNo;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Long getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(Long mobileNo) {
+		this.mobileNo = mobileNo;
+	}
 	
 	
 }
