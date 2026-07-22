c99ef3a89f1557939bda0c18a86b0dfee7f8f8e3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-22 21:07:51 +0530

    Even Numbers in the list

--- java8ProgramsPractice/src/com/day17/java8ProgramsPractice/EvenNumbers.java
+++ java8ProgramsPractice/src/com/day17/java8ProgramsPractice/EvenNumbers.java
@@ -10,8 +10,13 @@ public class EvenNumbers {
 		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10,
 				11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26);
 		
+		
+		
 		list.stream()
 		.filter(a -> a % 2 == 0)
 		.forEach(System.out::println);
+		
+		
+		
 	}
 }
