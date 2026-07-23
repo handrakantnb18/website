ebfa486abe3bf77baac322658d540d875a74020f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-23 23:19:37 +0530

    Employee private variable and toString methods

---
+++ zeroToHero/src/com/zeroToHero/core/Employee.java
@@ -0,0 +1,22 @@
+package com.zeroToHero.core;
+
+public class Employee {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String city;
+	
+	private String dept;
+	
+	private Double mobileNo;
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", city=" + city + ", dept=" + dept + ", mobileNo=" + mobileNo
+				+ "]";
+	}
+	
+	
+}
