27aa1719c2d42d2edba5f15ab7a2e7cf5262dfbe
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-23 23:20:23 +0530

    getter and setter method added constructor

--- zeroToHero/src/com/zeroToHero/core/Employee.java
+++ zeroToHero/src/com/zeroToHero/core/Employee.java
@@ -17,6 +17,55 @@ public class Employee {
 		return "Employee [id=" + id + ", name=" + name + ", city=" + city + ", dept=" + dept + ", mobileNo=" + mobileNo
 				+ "]";
 	}
+
+	public Employee(Integer id, String name, String city, String dept, Double mobileNo) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+		this.dept = dept;
+		this.mobileNo = mobileNo;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public Double getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(Double mobileNo) {
+		this.mobileNo = mobileNo;
+	}
 	
 	
 }
