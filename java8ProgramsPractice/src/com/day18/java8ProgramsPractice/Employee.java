68d834e9e8d2577757077fbf48436eb9933751f1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-24 22:54:15 +0530

    constructor andm setter and getter method added

--- java8ProgramsPractice/src/com/day18/java8ProgramsPractice/Employee.java
+++ java8ProgramsPractice/src/com/day18/java8ProgramsPractice/Employee.java
@@ -17,6 +17,59 @@ public class Employee {
 		return "Employee [id=" + id + ", name=" + name + ", dept=" + dept + ", email=" + email + ", mobileNo="
 				+ mobileNo + "]";
 	}
+
+	
+	
+	public Employee(Integer id, String name, String dept, String email, String mobileNo) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dept = dept;
+		this.email = email;
+		this.mobileNo = mobileNo;
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
 	
 	
 	
