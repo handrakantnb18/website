186d8c2279a26de583801d19448ea58718963371
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-24 10:29:34 +0530

    The forEach() method internally uses the Consumer<T> functional interface, which takes one argument and performs an action.

---
+++ java8ProgramsPractice/src/com/day18/java8ProgramsPractice/AllNumAndEvanNumbers.java
@@ -0,0 +1,26 @@
+package com.day18.java8ProgramsPractice;
+
+import java.util.ArrayList;
+
+public class AllNumAndEvanNumbers {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(11);
+		list.add(12);
+		list.add(13);
+		list.add(14);
+		list.add(15);
+		
+		System.out.println("All Elements : ");
+		list.forEach(n -> System.out.println(n));
+		 
+		System.out.println("Even Numbers : ");
+		list.forEach(n -> {
+			if(n % 2 == 0)
+				System.out.println(n);
+			
+		});
+	}
+}
