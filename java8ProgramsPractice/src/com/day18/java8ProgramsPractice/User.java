87c1baa471e06d73217478b1cc8d83b0c0b235d7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-24 22:49:14 +0530

    User class private and toString name

---
+++ java8ProgramsPractice/src/com/day18/java8ProgramsPractice/User.java
@@ -0,0 +1,19 @@
+package com.day18.java8ProgramsPractice;
+
+public class User {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String dept;
+	
+	private String email;
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", dept=" + dept + ", email=" + email + "]";
+	}
+	
+	
+}
