8d5d072252e8d87dacbaead273a54f0718b59fb7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-24 23:02:27 +0530

    Second Heighest Number in the list

---
+++ java8ProgramsPractice/src/com/day18/java8ProgramsPractice/SecondHeighestNumber.java
@@ -0,0 +1,25 @@
+package com.day18.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SecondHeighestNumber {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(
+				78,98,65,25,5,35,199,25,48,32,15);
+		
+		Integer second = 
+				list.stream()
+				.distinct()
+				.sorted(Collections.reverseOrder())
+				.skip(2)
+				.findFirst()
+				.get();
+		
+		System.out.println(second);
+		
+	}
+}
