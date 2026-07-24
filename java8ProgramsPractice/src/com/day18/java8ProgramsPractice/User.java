728773e6bdb4a0626a9d27070f9cc04bf264d3c7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-24 22:49:59 +0530

    constructor, getter and setter method

--- java8ProgramsPractice/src/com/day18/java8ProgramsPractice/User.java
+++ java8ProgramsPractice/src/com/day18/java8ProgramsPractice/User.java
@@ -14,6 +14,46 @@ public class User {
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", dept=" + dept + ", email=" + email + "]";
 	}
+
+	public User(Integer id, String name, String dept, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dept = dept;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 	
 }
