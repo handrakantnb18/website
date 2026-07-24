fcbfa82e3585b9e23962c42024bb0413e6435f43
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-24 22:40:45 +0530

    getter and setter method and constructor

--- java8ProgramsPractice/src/com/day18/java8ProgramsPractice/Bank.java
+++ java8ProgramsPractice/src/com/day18/java8ProgramsPractice/Bank.java
@@ -14,6 +14,52 @@ public class Bank {
 	public String toString() {
 		return "Bank [id=" + id + ", dept=" + dept + ", name=" + name + ", mobileNo=" + mobileNo + "]";
 	}
+
+	public Bank(Integer id, String dept, String name, String mobileNo) {
+		super();
+		this.id = id;
+		this.dept = dept;
+		this.name = name;
+		this.mobileNo = mobileNo;
+	}
+
+	public Bank() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
 	
 	
 }
