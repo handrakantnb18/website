1a923c29231c7c74e6c6b6c98b89e3c60989737e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-26 20:48:58 +0530

    How do you find sum of first 10 natural numbers?

---
+++ java8ProgramsPractice/src/com/day20/java8ProgramsPractice/SumTenNaturalNum.java
@@ -0,0 +1,16 @@
+package com.day20.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SumTenNaturalNum {
+
+	public static void main(String[] args) {
+		
+		// List<Integer> num = Arrays.asList();
+		
+		// int sum = num.range(1, 11).sum();
+         
+	       // System.out.println(sum);
+	}
+}
