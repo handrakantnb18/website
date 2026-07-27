1a5e4b7ab956935f36f1217dd101676d9efb9105
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-27 08:34:53 +0530

    private variables and constructor

---
+++ java8ProgramsPractice/src/com/day21/java8ProgramsPractice/Students.java
@@ -0,0 +1,28 @@
+package com.day21.java8ProgramsPractice;
+
+public class Students {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String college;
+	
+	private String email;
+	
+	private String mobileNo;
+	
+	private String city;
+
+	public Students(Integer id, String name, String college, String email, String mobileNo, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.college = college;
+		this.email = email;
+		this.mobileNo = mobileNo;
+		this.city = city;
+	}
+	
+	
+}
