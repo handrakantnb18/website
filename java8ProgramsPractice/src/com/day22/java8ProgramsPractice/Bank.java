9887c6b4aecc7b0a1c3cc42967ee70b99a613ef5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-29 13:41:06 +0530

    getter and setter method and constructor used

--- java8ProgramsPractice/src/com/day22/java8ProgramsPractice/Bank.java
+++ java8ProgramsPractice/src/com/day22/java8ProgramsPractice/Bank.java
@@ -21,6 +21,73 @@ public class Bank {
 		return "Bank [id=" + id + ", name=" + name + ", email=" + email + ", mobile=" + mobile + ", branch=" + branch
 				+ ", accountType=" + accountType + ", balance=" + balance + "]";
 	}
+
+	public Bank(Integer id, String name, String email, Long mobile, String branch, String accountType, Double balance) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.mobile = mobile;
+		this.branch = branch;
+		this.accountType = accountType;
+		this.balance = balance;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(Long mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getBranch() {
+		return branch;
+	}
+
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
 	
 	
 	
