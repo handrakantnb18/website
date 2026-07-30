fd8785744b7fd4b702aa2ea62f31481849cb7a60
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-30 09:22:20 +0530

    spring boot practice all interview questions 100

---
+++ AllSpringAPIs/src/main/java/com/allspringApis/practice/AllSpringApIsApplication.java
@@ -0,0 +1,13 @@
+package com.allspringApis.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllSpringApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllSpringApIsApplication.class, args);
+	}
+
+}
---
+++ AllSpringAPIs/src/test/java/com/allspringApis/practice/AllSpringApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allspringApis.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllSpringApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
