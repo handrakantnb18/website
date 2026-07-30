c742bfc260de6f511d9cd4d3e01e2a2bd1f86902
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-30 22:45:39 +0530

    Caractor Frequency check the word into String

---
+++ java8ProgramsPractice/src/com/day23/java8ProgramsPractice/CaractorFrequency.java
@@ -0,0 +1,23 @@
+package com.day23.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CaractorFrequency {
+
+	public static void main(String[] args) {
+		
+		String str = "my name is Ram";
+		
+		Map<String, Long> map =
+				Arrays.stream(str.split(" "))
+				.collect(Collectors.groupingBy(
+						Function.identity(),
+						Collectors.counting()));
+				
+				
+				System.out.println(map);
+	}
+}
