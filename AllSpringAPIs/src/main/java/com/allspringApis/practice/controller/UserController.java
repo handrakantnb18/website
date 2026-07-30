7d407204cf0b7dab7ef8fe43c40ebb4dce3534ce
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-30 13:20:33 +0530

    @ExceptionHandler(Exception.class)	public ResponseEntity<String> handleAll(Exception ex){

--- AllSpringAPIs/src/main/java/com/allspringApis/practice/controller/UserController.java
+++ AllSpringAPIs/src/main/java/com/allspringApis/practice/controller/UserController.java
@@ -4,10 +4,17 @@ import java.util.List;
 
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.allspringApis.practice.services.UserServices;
 
+@ControllerAdvice
 @RestController
 public class UserController {
 
@@ -21,5 +28,16 @@ public class UserController {
 	//public List<User> getUsers() {
 	//	return UserServices.userServices();
 	//}
+	
+	@ExceptionHandler(ConfigDataResourceNotFoundException.class)
+	public ResponseEntity<String> handleNotFound(ConfigDataResourceNotFoundException ex){
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handleAll(Exception ex){
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error : " + ex.getMessage());
+		
+	}
 	
 }
