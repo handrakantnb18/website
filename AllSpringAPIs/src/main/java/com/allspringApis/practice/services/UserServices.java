cdeb982fb3ca4d33d8ff499cb092e6dc1982567a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-30 12:55:05 +0530

    UserServices	public List<User> getAllUsers() {

---
+++ AllSpringAPIs/src/main/java/com/allspringApis/practice/services/UserServices.java
@@ -0,0 +1,14 @@
+package com.allspringApis.practice.services;
+
+import java.util.List;
+
+import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserServices {
+
+	public List<User> getAllUsers() {
+		return getAllUsers();
+	}
+}
