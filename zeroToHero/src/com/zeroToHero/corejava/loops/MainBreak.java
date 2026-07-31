a94790af24c803d9dae132311cef3f3f0e3eabba
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-31 10:43:56 +0530

    break statement example with labeled for loop

---
+++ zeroToHero/src/com/zeroToHero/corejava/loops/MainBreak.java
@@ -0,0 +1,23 @@
+package com.zeroToHero.corejava.loops;
+
+public class MainBreak {
+
+	public static void main(String[] args) {
+		
+	    a:      
+	        for(int i = 0; i<= 10; i++) {      
+	            b:      
+	            for(int j = 0; j<=15;j++) {      
+	                c:      
+	                for (int k = 0; k<=20; k++) {      
+	                    System.out.println(k);      
+	                    if(k==5) {      
+	                    break a;      
+	                    }      
+	                }      
+	            }      
+	          
+	        }      
+
+	}
+}
