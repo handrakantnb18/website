31f1ae64a6fb8a53d3780d91b54e005ffe3b6774
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-31 10:16:02 +0530

    we have used the if statement in the java code.

---
+++ zeroToHero/src/com/zeroToHero/corejava/loops/MainIf.java
@@ -0,0 +1,16 @@
+package com.zeroToHero.corejava.loops;
+
+public class MainIf {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		
+		int b = 20;
+		
+		if(a+b > 20)
+		{
+			System.out.println("a+b is greater than 20.");
+		}
+	}
+}
