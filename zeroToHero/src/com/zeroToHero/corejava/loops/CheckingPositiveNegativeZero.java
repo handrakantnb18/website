1825af4188600cdcaed20a9ebfc801222287d8ee
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-31 13:53:49 +0530

    Checking Positive, Negative, or Zero, Let us take an example to demonstrate how to check positive, negative, and zero using the if else-if ladder statement in Java.

---
+++ zeroToHero/src/com/zeroToHero/corejava/loops/CheckingPositiveNegativeZero.java
@@ -0,0 +1,26 @@
+package com.zeroToHero.corejava.loops;
+
+public class CheckingPositiveNegativeZero {
+
+	public static void main(String[] args) {
+		
+//		int num = 77;
+		
+//		int num = 0;
+
+		int num = -77;
+		
+		if(num > 0)
+		{
+			System.out.println("Positive Number");
+		}
+		else if(num < 0)
+		{
+			System.out.println("Nigative Number");
+		}
+		else
+		{
+			System.out.println("Zero");
+		}
+	}
+}
