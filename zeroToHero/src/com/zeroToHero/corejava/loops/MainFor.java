23fc70d8d86aa0db396316295adc061a94ca6ce1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-31 10:35:35 +0530

    The num of first 10 natural numbers is = 55 Consider the following example to understand the proper functioning of the for loop in java.

---
+++ zeroToHero/src/com/zeroToHero/corejava/loops/MainFor.java
@@ -0,0 +1,18 @@
+package com.zeroToHero.corejava.loops;
+
+public class MainFor {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub    
+		
+		int num = 0;
+		
+		for(int j = 1; j<=10;j++) {
+			num = num+j;
+		}
+		
+		System.out.println("The num of first 10 natural numbers is : "+num);
+		
+	}
+}
