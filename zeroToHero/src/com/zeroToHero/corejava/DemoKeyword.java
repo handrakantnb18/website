ec1ce7502e46fdf7c10ea7a9e5bebe66a2fc3d63
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-31 10:12:40 +0530

    Number is greater than 5

---
+++ zeroToHero/src/com/zeroToHero/corejava/DemoKeyword.java
@@ -0,0 +1,16 @@
+package com.zeroToHero.corejava;
+
+public class DemoKeyword {
+
+	public static void main(String[] args) {
+		
+		int number = 10;
+		
+		// Number is greater than 5
+		if(number > 5)
+		{
+			System.out.println("Number is greater than 5");
+		}
+		
+	}
+}
