cdffe3545adcec665cc3dda83c3d3fff7868f076
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-31 10:17:40 +0530

    The if-else statement is an extension to the if-statement, which uses another block of code

--- zeroToHero/src/com/zeroToHero/corejava/loops/MainIf.java
+++ zeroToHero/src/com/zeroToHero/corejava/loops/MainIf.java
@@ -4,13 +4,29 @@ public class MainIf {
 
 	public static void main(String[] args) {
 		
+//		int a = 10;
+//		
+//		int b = 20;
+//		
+//		if(a+b > 20)
+//		{
+//			System.out.println("a+b is greater than 20.");
+//		}
+		
+		
 		int a = 10;
 		
 		int b = 20;
 		
-		if(a+b > 20)
+		if(a+b < 20)
 		{
 			System.out.println("a+b is greater than 20.");
 		}
+		else
+		{
+			System.out.println("a+b is less than 20");
+		}
+		
+		
 	}
 }
