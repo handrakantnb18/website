afe78d6585e8eff6a5803857f8d56697e88d201a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-31 14:17:24 +0530

    Checking Elegibility for voting

---
+++ zeroToHero/src/com/zeroToHero/corejava/loops/CheckingAgeVoting.java
@@ -0,0 +1,21 @@
+package com.zeroToHero.corejava.loops;
+
+public class CheckingAgeVoting {
+
+	public static void main(String[] args) {
+		
+//		int age = 15;
+
+		int age = 19;
+		
+//		int weight = 75;
+		
+		if(age>= 18) {
+			
+//			if(weight>70) {
+//				System.out.println();
+//			}
+			System.out.println("Your are Eligible for Voting...");
+		}
+	}
+}
