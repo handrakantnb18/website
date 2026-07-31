65bc016a9755fba423056e54c10a1fffde6438ca
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-07-31 14:21:33 +0530

    We can also use ternary operator (? :) "even number":"odd number";

---
+++ zeroToHero/src/com/zeroToHero/corejava/loops/OddEvenNumber.java
@@ -0,0 +1,15 @@
+package com.zeroToHero.corejava.loops;
+
+public class OddEvenNumber {
+
+	public static void main(String[] args) {
+		
+//		int num = 15;
+		
+		int num = 16;
+
+		String output = (num % 2 == 0)?"Even number":"Odd Number";
+		System.out.println(output);
+		
+	}
+}
