93dc524a944ad78183ac1c9d383cf940fba8b079
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-01 12:30:25 +0530

    Java Program to illustrate how to define a class and fields instance variable, reference variable

---
+++ zeroToHero/src/com/zeroToHero/corejava/oopsConcepts/MainVariables.java
@@ -0,0 +1,19 @@
+package com.zeroToHero.corejava.oopsConcepts;
+
+public class MainVariables {
+
+	// instance variable
+	int id;
+	
+	String name;
+	
+	public static void main(String[] args) {
+		
+		MainVariables s1 = new MainVariables();
+		
+		System.out.println(s1.id); // reference variable
+		
+		System.out.println(s1.name); // reference variable
+		
+	}
+}
