7664db1ae2045ffbdd4b768047b7cbb0ce20544c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-01 12:36:13 +0530

    We can have multiple classes in different Java files or a single Java file. If you define multiple classes in a single Java source file

--- zeroToHero/src/com/zeroToHero/corejava/oopsConcepts/MainVariables.java
+++ zeroToHero/src/com/zeroToHero/corejava/oopsConcepts/MainVariables.java
@@ -1,11 +1,22 @@
 package com.zeroToHero.corejava.oopsConcepts;
 
+//Creating a Student class
+class Student1{
+	
+	 //declaring fields or instance variables  
+
+	int mobileNo = 987456123;
+	
+	String email = "chandrakant@gmail.com";
+	
+}
+
 public class MainVariables {
 
 	// instance variable
-	int id;
+	int id = 101;
 	
-	String name;
+	String name = "chandrakant ";
 	
 	public static void main(String[] args) {
 		
@@ -14,6 +25,14 @@ public class MainVariables {
 		System.out.println(s1.id); // reference variable
 		
 		System.out.println(s1.name); // reference variable
+		
+		//Creating another class which contains the main() method    
+
+		Student1 st = new Student1();
+		
+		System.out.println(st.email);
+		
+		System.out.println(st.mobileNo);
 		
 	}
 }
