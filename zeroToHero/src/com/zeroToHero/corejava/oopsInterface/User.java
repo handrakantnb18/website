ffff3ae906d45aedf2b7f7653eb9c885a782c004
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-02 23:54:39 +0530

    User class private variables and constructor created

---
+++ zeroToHero/src/com/zeroToHero/corejava/oopsInterface/User.java
@@ -0,0 +1,25 @@
+package com.zeroToHero.corejava.oopsInterface;
+
+public class User {
+
+	private Integer id;
+	
+	private String anme;
+	
+	private String mobileNo;
+	
+	private String email;
+	
+	private String city;
+
+	public User(Integer id, String anme, String mobileNo, String email, String city) {
+		super();
+		this.id = id;
+		this.anme = anme;
+		this.mobileNo = mobileNo;
+		this.email = email;
+		this.city = city;
+	}
+	
+	
+}
