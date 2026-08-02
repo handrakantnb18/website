aabc4dc04d7552d22a4689e7c4195b9a86f2e80b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-02 23:35:57 +0530

    getter and setter method and constructor created

--- zeroToHero/src/com/zeroToHero/corejava/oopsInterface/Student.java
+++ zeroToHero/src/com/zeroToHero/corejava/oopsInterface/Student.java
@@ -17,6 +17,55 @@ public class Student {
 		return "Student [id=" + id + ", name=" + name + ", salary=" + salary + ", experience=" + experience + ", dept="
 				+ dept + "]";
 	}
+
+	public Student(Integer id, String name, Double salary, Integer experience, String dept) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.experience = experience;
+		this.dept = dept;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public Integer getExperience() {
+		return experience;
+	}
+
+	public void setExperience(Integer experience) {
+		this.experience = experience;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
 	
 	
 }
