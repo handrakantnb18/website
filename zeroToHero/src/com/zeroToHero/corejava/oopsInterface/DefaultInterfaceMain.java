0a7b2f9c13d3c867ca8f59872a2ed961068eaddd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-02 11:37:13 +0530

    Default Interface Main created

---
+++ zeroToHero/src/com/zeroToHero/corejava/oopsInterface/DefaultInterfaceMain.java
@@ -0,0 +1,14 @@
+package com.zeroToHero.corejava.oopsInterface;
+
+public class DefaultInterfaceMain {
+
+	public void display() {
+		System.out.println("interface created");
+	}
+	public static void main(String[] args) {
+		
+		DefaultInterfaceMain d = new DefaultInterfaceMain();
+		
+		d.display();
+	}
+}
