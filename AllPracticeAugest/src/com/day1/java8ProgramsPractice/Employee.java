adbc9071e83d4888c01b0627bb34e06735375b47
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-03 00:11:57 +0530

    Employee class private variables and constructor

---
+++ AllPracticeAugest/src/com/day1/java8ProgramsPractice/Employee.java
@@ -0,0 +1,32 @@
+package com.day1.java8ProgramsPractice;
+
+public class Employee {
+
+	private Integer id;
+	
+	private String name;
+	
+	private Double salary;
+	
+	private Integer experience;
+	
+	private String dept;
+	
+	private String mobileNo;
+	
+	private String email;
+
+	public Employee(Integer id, String name, Double salary, Integer experience, String dept, String mobileNo,
+			String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.experience = experience;
+		this.dept = dept;
+		this.mobileNo = mobileNo;
+		this.email = email;
+	}
+	
+	
+}
