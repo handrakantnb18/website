ed3e7784fa35fe101cf12d2182cdc8ee01010af1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-02 23:55:53 +0530

    User class getter and setter method and toString method created

--- zeroToHero/src/com/zeroToHero/corejava/oopsInterface/User.java
+++ zeroToHero/src/com/zeroToHero/corejava/oopsInterface/User.java
@@ -20,6 +20,57 @@ public class User {
 		this.email = email;
 		this.city = city;
 	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", anme=" + anme + ", mobileNo=" + mobileNo + ", email=" + email + ", city=" + city
+				+ "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getAnme() {
+		return anme;
+	}
+
+	public void setAnme(String anme) {
+		this.anme = anme;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	
 	
 }
