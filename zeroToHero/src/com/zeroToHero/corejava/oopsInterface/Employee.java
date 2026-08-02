b2b7319c7b06c35a4d68c431a50672b8c229eacd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-02 22:34:14 +0530

    getter and setter method constructor used

--- zeroToHero/src/com/zeroToHero/corejava/oopsInterface/Employee.java
+++ zeroToHero/src/com/zeroToHero/corejava/oopsInterface/Employee.java
@@ -19,6 +19,69 @@ public class Employee {
 		return "Employee [empid=" + empid + ", empname=" + empname + ", mobileNo=" + mobileNo + ", email=" + email
 				+ ", city=" + city + ", dept=" + dept + "]";
 	}
+
+	public Employee(Integer empid, String empname, String mobileNo, String email, String city, String dept) {
+		super();
+		this.empid = empid;
+		this.empname = empname;
+		this.mobileNo = mobileNo;
+		this.email = email;
+		this.city = city;
+		this.dept = dept;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getEmpid() {
+		return empid;
+	}
+
+	public void setEmpid(Integer empid) {
+		this.empid = empid;
+	}
+
+	public String getEmpname() {
+		return empname;
+	}
+
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
 	
 	
 }
