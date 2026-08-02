6b7e00c5d4e79e2459083c92ffe976bff031377a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-02 23:50:55 +0530

    Sort Salary Descending and Aschending order

---
+++ zeroToHero/src/com/zeroToHero/corejava/oopsInterface/StudentEmployeeDescending.java
@@ -0,0 +1,40 @@
+package com.zeroToHero.corejava.oopsInterface;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class StudentEmployeeDescending {
+
+	public static void main(String[] args) {
+		
+List<Student> list = Arrays.asList(
+				
+				new Student(101, "Amit", 55000.0, 3, "IT"),
+			    new Student(102, "Rahul", 85000.0, 7, "Computer"),
+			    new Student(103, "Priya", 95000.0, 8, "Electronics"),
+			    new Student(104, "Sneha", 45000.0, 2, "Mechanical"),
+			    new Student(105, "Vikas", 70000.0, 5, "Civil"),
+			    new Student(106, "Pooja", 65000.0, 4, "IT"),
+			    new Student(107, "Rohit", 120000.0, 10, "Computer"),
+			    new Student(108, "Neha", 50000.0, 2, "Electrical"),
+			    new Student(109, "Kiran", 98000.0, 9, "Electronics"),
+			    new Student(110, "Anjali", 60000.0, 5, "Civil")
+			    
+				);
+		
+			System.out.println("Descending Order : ");
+
+			list.stream()
+			.sorted(Comparator.comparing(Student::getSalary).reversed())
+			.forEach(System.out::println);
+
+			System.out.println("===================================================");
+			System.out.println("Aschending Order : ");
+			
+			list.stream()
+			.sorted(Comparator.comparing(Student::getSalary))
+			.forEach(System.out::println);
+
+	}
+}
