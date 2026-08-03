c16720cdffe7e1952dc5c8dd2005c351d567e4aa
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-03 17:17:06 +0530

    User clas  private variables and toString mrthod

---
+++ AllPracticeAugest/src/com/day2/java8ProgramsPractice/User.java
@@ -0,0 +1,25 @@
+package com.day2.java8ProgramsPractice;
+
+public class User {
+
+	private Integer uid;
+	
+	private String uname;
+	
+	private String uemail;
+	
+	private Double usalary;
+	
+	private String mobile;
+	
+	private String address;
+
+	@Override
+	public String toString() {
+		return "User [uid=" + uid + ", uname=" + uname + ", uemail=" + uemail + ", usalary=" + usalary + ", mobile="
+				+ mobile + ", address=" + address + "]";
+	}
+	
+	
+	
+}
