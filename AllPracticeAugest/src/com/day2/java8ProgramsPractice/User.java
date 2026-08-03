b5763bf74cf5669ef53afad0268329e540189089
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-03 17:22:56 +0530

    getter and setter method are created and constructor

--- AllPracticeAugest/src/com/day2/java8ProgramsPractice/User.java
+++ AllPracticeAugest/src/com/day2/java8ProgramsPractice/User.java
@@ -19,6 +19,64 @@ public class User {
 		return "User [uid=" + uid + ", uname=" + uname + ", uemail=" + uemail + ", usalary=" + usalary + ", mobile="
 				+ mobile + ", address=" + address + "]";
 	}
+
+	public Integer getUid() {
+		return uid;
+	}
+
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getUemail() {
+		return uemail;
+	}
+
+	public void setUemail(String uemail) {
+		this.uemail = uemail;
+	}
+
+	public Double getUsalary() {
+		return usalary;
+	}
+
+	public void setUsalary(Double usalary) {
+		this.usalary = usalary;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public User(Integer uid, String uname, String uemail, Double usalary, String mobile, String address) {
+		super();
+		this.uid = uid;
+		this.uname = uname;
+		this.uemail = uemail;
+		this.usalary = usalary;
+		this.mobile = mobile;
+		this.address = address;
+	}
 	
 	
 	
