a3b85848d5e1a71292f772f70bfe5457bdf437c1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-03 19:03:17 +0530

    find smallest String in the Arrays of list

---
+++ AllPracticeAugest/src/com/day2/java8ProgramsPractice/ShortestString.java
@@ -0,0 +1,24 @@
+package com.day2.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class ShortestString {
+
+	// find smallest String in the Arrays of list
+	
+	public static void main(String[] args) {
+		
+		List<String> str = Arrays.asList(
+				"Software","chandrakant","Priyanka", "Shivraj");
+		
+		String smallest = 
+				str.stream()
+				.min(Comparator.comparing(String::length))
+				.get();
+		
+		System.out.println(smallest);
+		
+	}
+}
