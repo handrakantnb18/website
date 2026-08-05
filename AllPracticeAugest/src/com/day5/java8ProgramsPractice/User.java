296744dbc1623ee98824b5dda99e81420f5e5270
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-05 22:56:09 +0530

    User class with 7 variables: and constructor

---
+++ AllPracticeAugest/src/com/day5/java8ProgramsPractice/User.java
@@ -0,0 +1,31 @@
+package com.day5.java8ProgramsPractice;
+
+public class User {
+
+	private Integer uid;
+	
+	private String name;
+	
+	private String email;
+	
+	private String mobileNo;
+	
+	private String city;
+	
+	private Integer age;
+	
+	private String gender;
+
+	public User(Integer uid, String name, String email, String mobileNo, String city, Integer age, String gender) {
+		super();
+		this.uid = uid;
+		this.name = name;
+		this.email = email;
+		this.mobileNo = mobileNo;
+		this.city = city;
+		this.age = age;
+		this.gender = gender;
+	}
+	
+	
+}
