3ea8f779c76471af48aeddd1b5db13a088b58736
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-05 22:57:03 +0530

    User class with 7 variables: getter and setter method

--- AllPracticeAugest/src/com/day5/java8ProgramsPractice/User.java
+++ AllPracticeAugest/src/com/day5/java8ProgramsPractice/User.java
@@ -26,6 +26,74 @@ public class User {
 		this.age = age;
 		this.gender = gender;
 	}
+
+	@Override
+	public String toString() {
+		return "User [uid=" + uid + ", name=" + name + ", email=" + email + ", mobileNo=" + mobileNo + ", city=" + city
+				+ ", age=" + age + ", gender=" + gender + "]";
+	}
+
+	
+	public Integer getUid() {
+		return uid;
+	}
+
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	
 	
 }
