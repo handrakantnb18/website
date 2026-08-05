b2f3be1f7a8196224a70381103680ef42f1145e6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-05 21:00:11 +0530

    Student class getter and setter method and toString method

--- AllPracticeAugest/src/com/day4/java8ProgramsPractice/Student.java
+++ AllPracticeAugest/src/com/day4/java8ProgramsPractice/Student.java
@@ -27,6 +27,73 @@ public class Student {
 		this.email = email;
 		this.city = city;
 	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", collage=" + collage + ", address=" + address + ", mobileNo="
+				+ mobileNo + ", email=" + email + ", city=" + city + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCollage() {
+		return collage;
+	}
+
+	public void setCollage(String collage) {
+		this.collage = collage;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	
 	
 }
