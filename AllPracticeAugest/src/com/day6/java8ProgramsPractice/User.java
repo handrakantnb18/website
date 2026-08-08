961b0fbdd4d08924cb304df4971697faa3a1b1a8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-08 14:22:45 +0530

    constructor and getter and setter method used

--- AllPracticeAugest/src/com/day6/java8ProgramsPractice/User.java
+++ AllPracticeAugest/src/com/day6/java8ProgramsPractice/User.java
@@ -19,6 +19,64 @@ public class User {
 		return "User [id=" + id + ", name=" + name + ", email=" + email + ", mobileNo=" + mobileNo + ", gender="
 				+ gender + ", city=" + city + "]";
 	}
+
+	public User(Integer id, String name, String email, String mobileNo, String gender, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.mobileNo = mobileNo;
+		this.gender = gender;
+		this.city = city;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
 	
 	
 	
