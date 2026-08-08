801371204bdcba4f5697a13fa2f7a69ded8bf62b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-08 22:53:42 +0530

    Employee class variables and toString methods used

---
+++ AllPracticeAugest/src/com/day7/java8ProgramsPractice/Employee.java
@@ -0,0 +1,29 @@
+package com.day7.java8ProgramsPractice;
+
+public class Employee {
+
+	private Integer empId;
+	
+	private String empName;
+	
+	private String email;
+	
+	private String dept;
+	
+	private String mobileNo;
+	
+	private Double salary;
+	
+	private String city;
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", email=" + email + ", dept=" + dept
+				+ ", mobileNo=" + mobileNo + ", salary=" + salary + ", city=" + city + "]";
+	}
+	
+	
+	
+	
+	
+}
