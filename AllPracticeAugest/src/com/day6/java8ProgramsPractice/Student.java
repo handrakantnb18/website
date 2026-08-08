9806e10fc90a77ef051c1a5488b0c0687ad714c3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-08 12:16:49 +0530

    Student class with 7 variables, including constructor, getters/setters, and toString():

---
+++ AllPracticeAugest/src/com/day6/java8ProgramsPractice/Student.java
@@ -0,0 +1,28 @@
+package com.day6.java8ProgramsPractice;
+
+public class Student {
+
+	private Integer stdId;
+	
+	private String name;
+	
+	private String email;
+	
+	private String mobileMo;
+	
+	private String city;
+	
+	private String course;
+	
+	private Double marks;
+
+	@Override
+	public String toString() {
+		return "Student [stdId=" + stdId + ", name=" + name + ", email=" + email + ", mobileMo=" + mobileMo + ", city="
+				+ city + ", course=" + course + ", marks=" + marks + "]";
+	}
+	
+	
+	
+	
+}
