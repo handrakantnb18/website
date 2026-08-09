d45668634816acf0d991b9e988739cc2f95c6920
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-09 21:57:47 +0530

    Map<Integer, Employee> containing 10 employees based on your 7 variables.

---
+++ AllPracticeAugest/src/com/day8/java8ProgramsPractice/Employee.java
@@ -0,0 +1,27 @@
+package com.day8.java8ProgramsPractice;
+
+public class Employee {
+
+	private Integer empid;
+	
+	private String name;
+	
+	private String email;
+	
+	private String dept;
+	
+	private String address;
+	
+	private String mobileNo;
+	
+	private String gender;
+
+	@Override
+	public String toString() {
+		return "Employee [empid=" + empid + ", name=" + name + ", email=" + email + ", dept=" + dept + ", address="
+				+ address + ", mobileNo=" + mobileNo + ", gender=" + gender + "]";
+	}
+	
+	
+	
+}
