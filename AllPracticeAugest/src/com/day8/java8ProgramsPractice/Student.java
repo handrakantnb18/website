27225f0939916ea0587cbd8ab6798f0663a578d7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-09 22:29:32 +0530

    User class with 7 variables and a Map<Integer, User> containing 10 users.

--- AllPracticeAugest/src/com/day8/java8ProgramsPractice/Student.java
+++ AllPracticeAugest/src/com/day8/java8ProgramsPractice/Student.java
@@ -31,7 +31,68 @@ public class Student {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", course=" + course + ", email=" + email + ", mobileNo="
+				+ mobileNo + ", dept=" + dept + ", address=" + address + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	
 	
 }
