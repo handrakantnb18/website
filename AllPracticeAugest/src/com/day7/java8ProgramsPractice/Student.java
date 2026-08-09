baa4800cf9a0a9221048895b7884648db808b88f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-09 18:33:21 +0530

    Student class private variables and toString methods

---
+++ AllPracticeAugest/src/com/day7/java8ProgramsPractice/Student.java
@@ -0,0 +1,26 @@
+package com.day7.java8ProgramsPractice;
+
+public class Student {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String cource;
+	
+	private String city;
+	
+	private Double fees;
+	
+	private String dept;
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", email=" + email + ", cource=" + cource + ", city=" + city
+				+ ", fees=" + fees + ", dept=" + dept + "]";
+	}
+	
+	
+}
