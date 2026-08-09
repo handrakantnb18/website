ba19cbdb0ec49363971b7dd3d2f098468fde795c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-09 13:53:04 +0530

    constructor and getter and setter method used

--- AllPracticeAugest/src/com/day7/java8ProgramsPractice/Employee.java
+++ AllPracticeAugest/src/com/day7/java8ProgramsPractice/Employee.java
@@ -21,9 +21,79 @@ public class Employee {
 		return "Employee [empId=" + empId + ", empName=" + empName + ", email=" + email + ", dept=" + dept
 				+ ", mobileNo=" + mobileNo + ", salary=" + salary + ", city=" + city + "]";
 	}
-	
-	
-	
+
+	public Employee(Integer empId, String empName, String email, String dept, String mobileNo, Double salary,
+			String city) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.email = email;
+		this.dept = dept;
+		this.mobileNo = mobileNo;
+		this.salary = salary;
+		this.city = city;
+	}
+
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	
 	
 }
