06e09a76db3d3b872beba419e4dd8beb08e6da7b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-09 22:00:19 +0530

    containing 10 employees based on your 7 variables. constructor getter and setter method

--- AllPracticeAugest/src/com/day8/java8ProgramsPractice/Employee.java
+++ AllPracticeAugest/src/com/day8/java8ProgramsPractice/Employee.java
@@ -21,7 +21,78 @@ public class Employee {
 		return "Employee [empid=" + empid + ", name=" + name + ", email=" + email + ", dept=" + dept + ", address="
 				+ address + ", mobileNo=" + mobileNo + ", gender=" + gender + "]";
 	}
-	
-	
+
+	public Integer getEmpid() {
+		return empid;
+	}
+
+	public void setEmpid(Integer empid) {
+		this.empid = empid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Employee(Integer empid, String name, String email, String dept, String address, String mobileNo,
+			String gender) {
+		super();
+		this.empid = empid;
+		this.name = name;
+		this.email = email;
+		this.dept = dept;
+		this.address = address;
+		this.mobileNo = mobileNo;
+		this.gender = gender;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	
 }
