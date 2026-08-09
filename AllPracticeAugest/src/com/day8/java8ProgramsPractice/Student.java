17e780b45dc6f6d43a4f1a54cdda6e118bb5746a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-09 22:28:42 +0530

    Student class private variables and constructor

---
+++ AllPracticeAugest/src/com/day8/java8ProgramsPractice/Student.java
@@ -0,0 +1,37 @@
+package com.day8.java8ProgramsPractice;
+
+public class Student {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String course;
+	
+	private String email;
+	
+	private String mobileNo;
+	
+	private String dept;
+	
+	private String address;
+
+	public Student(Integer id, String name, String course, String email, String mobileNo, String dept, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.course = course;
+		this.email = email;
+		this.mobileNo = mobileNo;
+		this.dept = dept;
+		this.address = address;
+	}
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
