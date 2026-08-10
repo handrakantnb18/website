346876573ecb644e15781a295407545663798c6a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-10 12:06:09 +0530

    Find duplicate characters in a String

---
+++ AllPracticeAugest/src/com/day9/java8ProgramsPractice/DuplicateCharactors.java
@@ -0,0 +1,29 @@
+package com.day9.java8ProgramsPractice;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class DuplicateCharactors {
+
+	public static void main(String[] args) {
+		
+		String str = "software enginner";
+		
+		Map<Character, Long> res =
+				str.chars()
+				.mapToObj(a -> (char) a)
+				.collect(Collectors.groupingBy(
+						Function.identity(),
+						Collectors.counting()));
+		
+		res.entrySet()
+		.stream()
+		.filter(entry -> entry.getValue() > 1)
+		.forEach(System.out::println);
+		
+		String str1 = "chandrakant bhosale";
+		
+		
+	}
+}
