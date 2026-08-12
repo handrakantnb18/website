1cc428feac25d567b0ba1888c833bc4bf0e2a7e8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-12 16:55:40 +0530

    7 variables in your Employee class, you can add address and gender.

---
+++ AllPracticeAugest/src/com/day12/java8ProgramsPractice/Employee.java
@@ -0,0 +1,27 @@
+package com.day12.java8ProgramsPractice;
+
+public class Employee {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String mobileNo;
+	
+	private String dept;
+	
+	private String address;
+	
+	private String gender;
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", mobileNo=" + mobileNo + ", dept="
+				+ dept + ", address=" + address + ", gender=" + gender + "]";
+	}
+	
+	
+	
+}
