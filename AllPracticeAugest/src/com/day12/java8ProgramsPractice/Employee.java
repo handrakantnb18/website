9b5bd2a08e75d2552673156c32538e29f483508d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-12 16:57:30 +0530

    7 variables in your Employee class getter and setter method

--- AllPracticeAugest/src/com/day12/java8ProgramsPractice/Employee.java
+++ AllPracticeAugest/src/com/day12/java8ProgramsPractice/Employee.java
@@ -21,7 +21,79 @@ public class Employee {
 		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", mobileNo=" + mobileNo + ", dept="
 				+ dept + ", address=" + address + ", gender=" + gender + "]";
 	}
-	
+
+	public Employee(Integer id, String name, String email, String mobileNo, String dept, String address,
+			String gender) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.mobileNo = mobileNo;
+		this.dept = dept;
+		this.address = address;
+		this.gender = gender;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	
 	
 }
