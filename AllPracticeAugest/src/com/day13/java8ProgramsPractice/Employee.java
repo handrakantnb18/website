4d023590e82ab818fdea2f903749ed123e387e4b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-12 20:56:26 +0530

    Employee class with 7 variables, useful for Java 8 Stream API / Map getter and setter method and constructor

--- AllPracticeAugest/src/com/day13/java8ProgramsPractice/Employee.java
+++ AllPracticeAugest/src/com/day13/java8ProgramsPractice/Employee.java
@@ -21,6 +21,74 @@ public class Employee {
 		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", mobileNo=" + mobileNo + ", dept="
 				+ dept + ", address=" + address + ", salary=" + salary + "]";
 	}
+
+	public Employee(Integer id, String name, String email, String mobileNo, String dept, String address,
+			Double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.mobileNo = mobileNo;
+		this.dept = dept;
+		this.address = address;
+		this.salary = salary;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
 	
 	
 }
