8d0c09883f20edbf4ef1c59aa74ddd2d64555261
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-14 09:35:05 +0530

    Employee class getter and setter method and constructor

--- AllPracticeAugest/src/com/day14/java8ProgramsPractice/Employee.java
+++ AllPracticeAugest/src/com/day14/java8ProgramsPractice/Employee.java
@@ -21,6 +21,74 @@ public class Employee {
 		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", address=" + address + ", mobileNo="
 				+ mobileNo + ", salary=" + salary + ", dept=" + dept + "]";
 	}
+
+	public Employee(Integer id, String name, String email, String address, String mobileNo, Double salary,
+			String dept) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.address = address;
+		this.mobileNo = mobileNo;
+		this.salary = salary;
+		this.dept = dept;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
 	
 	
 	
