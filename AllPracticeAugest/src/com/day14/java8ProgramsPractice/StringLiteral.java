b2d6bb878d15a93fb8406c23029606e07df7b5f1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-14 14:53:58 +0530

    demonstrate how to find string length. find a character in a string how to compare strings.

--- AllPracticeAugest/src/com/day14/java8ProgramsPractice/StringLiteral.java
+++ AllPracticeAugest/src/com/day14/java8ProgramsPractice/StringLiteral.java
@@ -16,6 +16,27 @@ public class StringLiteral {
 		String s4 = new String("Welcome");
 		System.out.println(s3+" "+s4);
 		
+		// demonstrate how to find string length.
+		String s5 = "chandrakant bhosale";
+		System.out.println("Lenght of string : "+s5.length());
+		
+		// find a character in a string
+		String s6 = "i an form pune";
+		System.out.println("Index of 'p' : "+s6.indexOf('p'));
+		
+		// how to compare strings.
+		String s7 = "Apple";
+		String s8 = "Banana";
+		
+		System.out.println("Equals() method : "+s7.equals(s8));
+		System.out.println("CompareTo result : "+s7.compareTo(s8));
+		
+		String s9 = "Banana";
+		String s10 = "Banana";
+		
+		System.out.println("Equals() method : "+s9.equals(s10));
+		System.out.println("CompareTo result : "+s9.compareTo(s10));
+		
 		
 	}
 }
