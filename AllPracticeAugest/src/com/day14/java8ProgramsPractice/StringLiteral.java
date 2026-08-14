e7411168139dcae35dfbafcb6f2532a88d3af799
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-14 14:44:53 +0530

    Java Program to create string using string literal Java Program to create string using new keyword

---
+++ AllPracticeAugest/src/com/day14/java8ProgramsPractice/StringLiteral.java
@@ -0,0 +1,21 @@
+package com.day14.java8ProgramsPractice;
+
+public class StringLiteral {
+
+	public static void main(String[] args) {
+		
+		//Java Program to create string using string literal
+		// literal
+		String s1 = "Welcome";
+		String s2 = "welcome";
+		System.out.println(s1+" "+s2);
+		
+		//Java Program to create string using new keyword  
+		// new keyword
+		String s3 = new String("Welcome");
+		String s4 = new String("Welcome");
+		System.out.println(s3+" "+s4);
+		
+		
+	}
+}
