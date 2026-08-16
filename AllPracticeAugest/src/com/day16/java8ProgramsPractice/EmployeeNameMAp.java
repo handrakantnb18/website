bff242d0c1c1630d922c38e9304137d67d1c58e4
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-16 12:03:34 +0530

    Print employees from IT department Print employees having salary greater than 50000

--- AllPracticeAugest/src/com/day16/java8ProgramsPractice/EmployeeNameMAp.java
+++ AllPracticeAugest/src/com/day16/java8ProgramsPractice/EmployeeNameMAp.java
@@ -10,30 +10,45 @@ public class EmployeeNameMAp {
 		
 		Map<Integer, Employee> map = new HashMap<Integer, Employee>();
 		
-		map.put(101, new Employee(101, "Rahul", "rahul@gmail.com", "IT", "50000", "9876543210", "Pune"));
+		map.put(101, new Employee(101, "Rahul", "rahul@gmail.com", "IT", 50000.00, "9876543210", "Pune"));
 
-        map.put(102, new Employee(102, "Amit", "amit@gmail.com", "HR", "45000", "9876543211", "Mumbai"));
+        map.put(102, new Employee(102, "Amit", "amit@gmail.com", "HR", 45000.00, "9876543211", "Mumbai"));
 
-        map.put(103, new Employee(103, "Sneha", "sneha@gmail.com", "Finance", "55000", "9876543212", "Nashik"));
+        map.put(103, new Employee(103, "Sneha", "sneha@gmail.com", "Finance", 55000.00, "9876543212", "Nashik"));
 
-        map.put(104, new Employee(104, "Priya", "priya@gmail.com", "IT", "60000", "9876543213", "Pune"));
+        map.put(104, new Employee(104, "Priya", "priya@gmail.com", "IT", 60000.00, "9876543213", "Pune"));
 
-        map.put(105, new Employee(105, "Vijay", "vijay@gmail.com", "Sales", "48000", "9876543214", "Kolhapur"));
+        map.put(105, new Employee(105, "Vijay", "vijay@gmail.com", "Sales", 48000.00, "9876543214", "Kolhapur"));
 
-        map.put(106, new Employee(106, "Neha", "neha@gmail.com", "HR", "52000", "9876543215", "Satara"));
+        map.put(106, new Employee(106, "Neha", "neha@gmail.com", "HR", 52000.00, "9876543215", "Satara"));
 
-        map.put(107, new Employee(107, "Rohit", "rohit@gmail.com", "IT", "65000", "9876543216", "Pune"));
+        map.put(107, new Employee(107, "Rohit", "rohit@gmail.com", "IT", 65000.00, "9876543216", "Pune"));
 
-        map.put(108, new Employee(108, "Pooja", "pooja@gmail.com", "Finance", "58000", "9876543217", "Sangli"));
+        map.put(108, new Employee(108, "Pooja", "pooja@gmail.com", "Finance", 58000.00, "9876543217", "Sangli"));
 
-        map.put(109, new Employee(109, "Akash", "akash@gmail.com", "Sales", "47000", "9876543218", "Solapur"));
+        map.put(109, new Employee(109, "Akash", "akash@gmail.com", "Sales", 47000.00, "9876543218", "Solapur"));
 
-        map.put(110, new Employee(110, "Kiran", "kiran@gmail.com", "IT", "70000", "9876543219", "Pune"));
+        map.put(110, new Employee(110, "Kiran", "kiran@gmail.com", "IT", 70000.00, "9876543219", "Pune"));
 		
-		map.forEach((id, name) -> {
-			System.out.println(id+ " : "+name);
-		});
+//		map.forEach((id, name) -> {
+//			System.out.println(id+ " : "+name);
+//		});
 		
+        // Print employees from IT department
+        map.forEach((id, emp) -> {
+        	if(emp.getDept().equals("IT")) {
+        		System.out.println(emp);
+        	}
+        });
+        
+        // Print employees having salary greater than 50000
+//        map.forEach((id, employee) -> {
+//            if (Double.parseDouble(employee.getSalary()) > 50000) {
+//                System.out.println(employee);
+//            }
+//        });
+        
+        
 		
 	}
 }
