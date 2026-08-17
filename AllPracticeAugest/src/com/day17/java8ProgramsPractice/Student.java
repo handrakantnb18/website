50b6b664cf1ed99070e742388263d3e586d8e519
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-17 10:01:36 +0530

    Student class constructor and getter and setter method

--- AllPracticeAugest/src/com/day17/java8ProgramsPractice/Student.java
+++ AllPracticeAugest/src/com/day17/java8ProgramsPractice/Student.java
@@ -21,6 +21,74 @@ public class Student {
 		return "Student [id=" + id + ", name=" + name + ", collage=" + collage + ", email=" + email + ", mobileNo="
 				+ mobileNo + ", marks=" + marks + ", address=" + address + "]";
 	}
+
+	public Student(Integer id, String name, String collage, String email, String mobileNo, Double marks,
+			String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.collage = collage;
+		this.email = email;
+		this.mobileNo = mobileNo;
+		this.marks = marks;
+		this.address = address;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCollage() {
+		return collage;
+	}
+
+	public void setCollage(String collage) {
+		this.collage = collage;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public Double getMarks() {
+		return marks;
+	}
+
+	public void setMarks(Double marks) {
+		this.marks = marks;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	
 	
 }
