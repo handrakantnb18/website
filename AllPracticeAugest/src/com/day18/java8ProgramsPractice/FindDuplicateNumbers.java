42343a3fcaacbf5c704aba7a0b0c6bdab600405b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-18 22:26:49 +0530

    fins the duplicate numbers in the list

---
+++ AllPracticeAugest/src/com/day18/java8ProgramsPractice/FindDuplicateNumbers.java
@@ -0,0 +1,22 @@
+package com.day18.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FindDuplicateNumbers {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(
+				12,35,64,78,92,37,89,56,24,35,64,24,56);
+		
+		Set<Integer> set = new HashSet<Integer>();
+		
+		list.stream()
+		.filter(s -> !set.add(s))
+		.forEach(System.out::println);
+		
+	}
+}
