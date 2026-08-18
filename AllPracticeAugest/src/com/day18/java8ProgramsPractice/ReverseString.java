885b6d75f2d8a5cd0c69a510f2283ba0dd17f3cf
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-18 22:16:22 +0530

    find Reverse String for given word

---
+++ AllPracticeAugest/src/com/day18/java8ProgramsPractice/ReverseString.java
@@ -0,0 +1,17 @@
+package com.day18.java8ProgramsPractice;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		String str = "chandrakant";
+		
+		String rev = "";
+		
+		for(int i = str.length() - 1; i >= 0; i--) {
+			rev += str.charAt(i);
+		}
+		
+		System.out.println(rev);
+	}
+}
