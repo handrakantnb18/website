57b25bcaa23a088357d5508d7aa1761c452eec59
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-18 22:22:32 +0530

    find the Charactor frequency in the String

---
+++ AllPracticeAugest/src/com/day18/java8ProgramsPractice/CharactorFrequency.java
@@ -0,0 +1,24 @@
+package com.day18.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CharactorFrequency {
+
+	//find the Charactor frequency in the String
+	public static void main(String[] args) {
+		
+		String str = "my name is chandrakant bhsoale and i am from sangli";
+		
+		Map<String, Long> map = 
+				Arrays.stream(str.split(""))
+				.collect(Collectors.groupingBy(
+						Function.identity(),
+						Collectors.counting()));
+		
+		System.out.println(map);
+		
+	}
+}
