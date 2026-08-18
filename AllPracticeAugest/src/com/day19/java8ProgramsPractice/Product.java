3028dd91882d25cea94df00c788d5c4d317d6d99
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-18 22:32:25 +0530

    Product class with 7 variables getter and setter method constructor

--- AllPracticeAugest/src/com/day19/java8ProgramsPractice/Product.java
+++ AllPracticeAugest/src/com/day19/java8ProgramsPractice/Product.java
@@ -21,6 +21,74 @@ public class Product {
 		return "Product [id=" + id + ", name=" + name + ", category=" + category + ", price=" + price + ", qty=" + qty
 				+ ", brand=" + brand + ", description=" + description + "]";
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Integer getQty() {
+		return qty;
+	}
+
+	public void setQty(Integer qty) {
+		this.qty = qty;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Product(Integer id, String name, String category, Double price, Integer qty, String brand,
+			String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.category = category;
+		this.price = price;
+		this.qty = qty;
+		this.brand = brand;
+		this.description = description;
+	}
 	
 	
 }
