1fe702f2e90d69a5fe19123f18bda0ef3e299eba
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-19 13:56:01 +0530

    getProductById, createProduct, updateProduct,deleteProduct

--- RestFul-web-services/src/main/java/com/product/application/controller/ProductController.java
+++ RestFul-web-services/src/main/java/com/product/application/controller/ProductController.java
@@ -2,15 +2,21 @@ package com.product.application.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.product.application.entity.Product;
 import com.product.application.service.ProductService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -25,6 +31,30 @@ public class ProductController {
 		return ResponseEntity.ok(productService.getAllProducts());
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Product> getProductById(@PathVariable Long id){
+		return ResponseEntity.ok(productService.getProductById(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product) 
+	{
+		Product created = productService.createProduct(product);
+		return ResponseEntity.status(HttpStatus.CREATED).body(created);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Product> updateProduct(@PathVariable Long id, @Valid @RequestBody Product product)
+	{
+		return ResponseEntity.ok(productService.updateProduct(id, product));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteProduct(@PathVariable Long id)
+	{
+		productService.deleteProduct(id);
+		return ResponseEntity.noContent().build();
+	}
 	
 	
 }
