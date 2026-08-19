470cdce6f3d49b9b753d7f8531415029cb57124f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-19 14:05:29 +0530

    getByCategory, searchByName method created GetMapping

--- RestFul-web-services/src/main/java/com/product/application/controller/ProductController.java
+++ RestFul-web-services/src/main/java/com/product/application/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.product.application.entity.Product;
@@ -56,5 +57,16 @@ public class ProductController {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@GetMapping("/category/{category}")
+	public ResponseEntity<List<Product>> getByCategory(@PathVariable String category)
+	{
+		return ResponseEntity.ok(productService.getProductByCategory(category));
+	}
+	
+	@GetMapping("/search")
+	public ResponseEntity<List<Product>> searchByName(@RequestParam String name)
+	{
+		return ResponseEntity.ok(productService.searchProductByName(name));
+	}
 	
 }
