c59e04ab5d3d992330673f219d8c46fd84808e5c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-20 22:12:22 +0530

    Map<Integer, Employee> where the key is Employee ID and the value is the Employee object.

---
+++ AllPracticeAugest/src/com/day19/java8ProgramsPractice/EmployeeMap.java
@@ -0,0 +1,38 @@
+package com.day19.java8ProgramsPractice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmployeeMap {
+
+	public static void main(String[] args) {
+		
+		Map<Integer, Employee> map = new HashMap<Integer, Employee>();
+		
+		 map.put(101, new Employee(101, "Rahul", "Pune", "IT", 55000.0, "rahul@gmail.com", "9876543210"));
+
+	     map.put(102, new Employee(102, "Amit", "Mumbai", "HR", 48000.0, "amit@gmail.com", "9876543211"));
+
+	     map.put(103, new Employee(103, "Priya", "Nashik", "Finance", 62000.0, "priya@gmail.com", "9876543212"));
+
+	     map.put(104, new Employee(104, "Sneha", "Pune", "IT", 70000.0, "sneha@gmail.com", "9876543213"));
+
+	     map.put(105, new Employee(105, "Vijay", "Sangli", "Sales", 45000.0, "vijay@gmail.com", "9876543214"));
+
+	     map.put(106, new Employee(106, "Neha", "Kolhapur", "HR", 52000.0, "neha@gmail.com", "9876543215"));
+
+	     map.put(107, new Employee(107, "Rohit", "Satara", "IT", 68000.0, "rohit@gmail.com", "9876543216"));
+
+	     map.put(108, new Employee(108, "Pooja", "Pune", "Finance", 59000.0, "pooja@gmail.com", "9876543217"));
+
+	     map.put(109, new Employee(109, "Suresh", "Mumbai", "Sales", 47000.0, "suresh@gmail.com", "9876543218"));
+
+	     map.put(110, new Employee(110, "Kiran", "Nashik", "IT", 75000.0, "kiran@gmail.com", "9876543219"));
+
+	     map.forEach((id, name) -> {
+	    	 System.out.println(id+" : "+name);
+	     });
+	     
+	     
+	}	
+}
