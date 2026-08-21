2a575adaed3337f16c8c224dd9c19f793f1d13f9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-21 22:24:14 +0530

    getter and setter method and constructor

--- AllPracticeAugest/src/com/day22/java8ProgramsPractice/User.java
+++ AllPracticeAugest/src/com/day22/java8ProgramsPractice/User.java
@@ -21,6 +21,74 @@ public class User {
 		return "User [id=" + id + ", uname=" + uname + ", email=" + email + ", address=" + address + ", country="
 				+ country + ", mobileNo=" + mobileNo + ", gender=" + gender + "]";
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public User(Integer id, String uname, String email, String address, String country, String mobileNo,
+			String gender) {
+		super();
+		this.id = id;
+		this.uname = uname;
+		this.email = email;
+		this.address = address;
+		this.country = country;
+		this.mobileNo = mobileNo;
+		this.gender = gender;
+	}
 	
 	
 }
