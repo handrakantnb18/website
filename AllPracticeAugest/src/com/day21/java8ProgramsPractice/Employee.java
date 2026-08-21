2d75b2ab7f7c7ef23fd4cff60ff60a9920cbb2b1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-21 10:53:46 +0530

    Employee class private variables and toString method

---
+++ AllPracticeAugest/src/com/day21/java8ProgramsPractice/Employee.java
@@ -0,0 +1,33 @@
+package com.day21.java8ProgramsPractice;
+
+public class Employee {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String dept;
+	
+	private Double salary;
+	
+	private String mobileNo;
+	
+	private String address;
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", dept=" + dept + ", salary=" + salary
+				+ ", mobileNo=" + mobileNo + ", address=" + address + "]";
+	}
+	
+	
+	
+//	CHANDRAKANT BHOSALE    MONEY   15 + 14 + 5 = 34  = 7
+//					       HEALTH  8 + 5 + 1 + 12 + 20 + 8 = 54  = 9
+//					       JOB     15  + 2 = 17  = 8
+//					       FAME    1  + 5 = 6
+
+	
+}
