88209ccda1b9c370974e9a00c6c031d75381c730
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-21 22:45:22 +0530

    find Even Odd Numbers in the list

---
+++ AllPracticeAugest/src/com/day22/java8ProgramsPractice/EvenOddNumbers.java
@@ -0,0 +1,24 @@
+package com.day22.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EvenOddNumbers {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,
+				12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30);
+		
+		System.out.println("Even Numbers : ");
+		list.stream()
+		.filter(e -> e % 2 == 0)
+		.forEach(System.out::println);
+		
+		System.out.println("Odd Numbers : ");
+		list.stream()
+		.filter(o -> o % 2 != 0)
+		.forEach(System.out::println);
+		
+	}
+}
