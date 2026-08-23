54133d140179287150ac76f3c21cbc7731918128
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-23 12:50:15 +0530

    GlobalTrust main class

---
+++ GlobalTrust/src/main/java/com/product/application/GlobalTrustApplication.java
@@ -0,0 +1,13 @@
+package com.product.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalTrustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalTrustApplication.class, args);
+	}
+
+}
