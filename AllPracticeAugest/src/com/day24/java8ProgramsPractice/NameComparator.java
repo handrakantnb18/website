b5fd2cd5f30002104548b61858c15622c2db5a9b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-24 14:10:05 +0530

    -public class NameComparator implements Comparator<StudentT> {

--- AllPracticeAugest/src/com/day24/java8ProgramsPractice/NameComparator.java
+++ AllPracticeAugest/src/com/day24/java8ProgramsPractice/NameComparator.java
@@ -24,32 +24,32 @@ class StudentT {
 //			return s1.name.compareTo(s2.name);
 //		}
 
-public class NameComparator implements Comparator<StudentT> {
-	
-	public static void main(String[] args) {
-		
-		ArrayList al=new ArrayList();  
-		al.add(new StudentT(101,"Vijay",23));  
-		al.add(new StudentT(106,"Ajay",27));  
-		al.add(new StudentT(105,"Jai",21));  
-		  
-		System.out.println("Sorting by Name");  
-		  
-		Collections.sort(al,new NameComparator());  
-		Iterator itr=al.iterator();  
-		while(itr.hasNext()){  
-		StudentT st=(StudentT)itr.next();  
-		System.out.println(st.rollno+" "+st.name+" "+st.age);  
-		}  
-		  
-		System.out.println("Sorting by age");  
-		  
-		Collections.sort(al,new AgeComparator());  
-		Iterator itr2=al.iterator();  
-		while(itr2.hasNext()){  
-		StudentT st=(StudentT)itr2.next();  
-		System.out.println(st.rollno+" "+st.name+" "+st.age);  
-		}  
-		
-	}
-}
+//public class NameComparator implements Comparator<StudentT> {
+//	
+//	public static void main(String[] args) {
+//		
+//		ArrayList al=new ArrayList();  
+//		al.add(new StudentT(101,"Vijay",23));  
+//		al.add(new StudentT(106,"Ajay",27));  
+//		al.add(new StudentT(105,"Jai",21));  
+//		  
+//		System.out.println("Sorting by Name");  
+//		  
+//		Collections.sort(al,new NameComparator());  
+//		Iterator itr=al.iterator();  
+//		while(itr.hasNext()){  
+//		StudentT st=(StudentT)itr.next();  
+//		System.out.println(st.rollno+" "+st.name+" "+st.age);  
+//		}  
+//		  
+//		System.out.println("Sorting by age");  
+//		  
+//		Collections.sort(al,new AgeComparator());  
+//		Iterator itr2=al.iterator();  
+//		while(itr2.hasNext()){  
+//		StudentT st=(StudentT)itr2.next();  
+//		System.out.println(st.rollno+" "+st.name+" "+st.age);  
+//		}  
+//		
+//	}
+//}
