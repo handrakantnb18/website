03a48bab214c5dcc9564e93d0f4fb0f1bfa73e7e
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-24 12:09:09 +0530

    creating (constructing) a string from the subset of a character array.

---
+++ AllPracticeAugest/src/com/day24/java8ProgramsPractice/CharArrayToString.java
@@ -0,0 +1,18 @@
+package com.day24.java8ProgramsPractice;
+
+public class CharArrayToString {
+
+	public static void main(String[] args) {
+		
+		char[] charArray = {'c','h','a','n','d','r','a','k','a','n','t'};
+		
+		String res = new String(charArray, 6, 5);
+		System.out.println(res);
+		
+		String str1 = "bhosale";
+		
+		System.out.println(str1);
+		
+		
+	}
+}
