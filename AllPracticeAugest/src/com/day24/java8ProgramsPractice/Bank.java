4e25ebcfb25acfc31b408b2720ec0957f2737c68
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-24 10:30:28 +0530

    Bank class private variables and toString method

---
+++ AllPracticeAugest/src/com/day24/java8ProgramsPractice/Bank.java
@@ -0,0 +1,27 @@
+package com.day24.java8ProgramsPractice;
+
+public class Bank {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String address;
+	
+	private String email;
+	
+	private String mobileNo;
+	
+	private Long accountNo;
+	
+	private String branch;
+
+	@Override
+	public String toString() {
+		return "Bank [id=" + id + ", name=" + name + ", address=" + address + ", email=" + email + ", mobileNo="
+				+ mobileNo + ", accountNo=" + accountNo + ", branch=" + branch + "]";
+	}
+	
+	
+	
+}
