bf85b7743992be57b96c393ddbd3367b79bbca26
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-25 14:58:52 +0530

    find Unique Elements in the list

---
+++ AllPracticeAugest/src/com/day25/java8ProgramsPractice/UniqueElements.java
@@ -0,0 +1,21 @@
+package com.day25.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UniqueElements {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(
+				10,15,15,20,35,30,35);
+		
+		List<Integer> uni =
+		list.stream()
+		.distinct()
+		.collect(Collectors.toList());
+		
+		System.out.println(uni);
+	}
+}
