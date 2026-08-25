78747e9cdbc1e92a35e2744211b4f7fb1b1bba69
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-25 14:50:55 +0530

    find max number in the list

---
+++ AllPracticeAugest/src/com/day25/java8ProgramsPractice/MaxNumber.java
@@ -0,0 +1,20 @@
+package com.day25.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MaxNumber {
+
+	public static void main(String[] args) {
+		
+		List<Integer> num = Arrays.asList(
+				78,99,15,25,45,85,88,35,66,12,92);
+		
+		Integer max =
+				num.stream()
+				.max(Integer::compare)
+				.get();
+		
+		System.out.println(max);
+	}
+}
