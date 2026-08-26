7849061edfabe90f9c5dd485dc5c82d109d6f2db
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-26 09:24:59 +0530

    MainForeinFunMemoryApi

---
+++ Java17AllFeatures/src/com/day1/java17features/MainForeinFunMemoryApi.java
@@ -0,0 +1,18 @@
+package com.day1.java17features;
+
+//import java.lang.foreign.MemorySegment;
+//import jdk.incubator.foreign.MemoryAccess;
+//import jdk.incubator.foreign.MemorySegment;
+//import jdk.incubator.foreign.ResourceScope;
+
+public class MainForeinFunMemoryApi {
+
+	public static void main(String[] args) {
+//        try (var scope = ResourceScope.newConfinedScope()) {
+//            MemorySegment seg = MemorySegment.allocateNative(8, scope);
+//            MemoryAccess.setLong(seg, 123456789L);
+//            long v = MemoryAccess.getLong(seg);
+//            System.out.println(v); // 123456789
+//        }
+    }
+}
