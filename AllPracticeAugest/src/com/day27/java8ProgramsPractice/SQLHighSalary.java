1dc3f079b28d7844a52081f01a3cb02fdfebe9e8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-27 14:18:42 +0530

    SQL: Second-Highest Salary in Each Department The most interview-friendly solution is using DENSE_RANK():

---
+++ AllPracticeAugest/src/com/day27/java8ProgramsPractice/SQLHighSalary.java
@@ -0,0 +1,25 @@
+package com.day27.java8ProgramsPractice;
+
+public class SQLHighSalary {
+
+	public static void main(String[] args) {
+		
+//		SELECT department,
+//			employee_name,
+//			salary
+//		FROM(
+//				SELECT department,
+//				employee_name,
+//				salary,
+//				DENSE_RANK() OVER(
+//						PARTITION BY department
+//						ORDER BY salary DESC
+//						) AS salary_rank
+//				FROM employee
+//				) t
+//		WHERE salary_rank = 2;
+	
+		
+		
+	}
+}
