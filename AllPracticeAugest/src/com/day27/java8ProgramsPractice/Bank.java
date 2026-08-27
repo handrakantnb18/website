d18c126c1c131f20670c91f723d05e7cd9d13927
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-27 10:30:59 +0530

    Bank class private variables getter and setter method

---
+++ AllPracticeAugest/src/com/day27/java8ProgramsPractice/Bank.java
@@ -0,0 +1,87 @@
+package com.day27.java8ProgramsPractice;
+
+public class Bank {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String mobileNo;
+	
+	private String address;
+	
+	private String branch;
+	
+	private String accountType;
+	
+	private Double balance;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getBranch() {
+		return branch;
+	}
+
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+	
+	
+	
+}
