b7dd2465206f74a79baa1112fce5537e4a46f84d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-27 13:11:06 +0530

    Employee objects, find the second-highest salary in each department.

---
+++ AllPracticeAugest/src/com/day27/java8ProgramsPractice/EmployeeStream.java
@@ -0,0 +1,22 @@
+package com.day27.java8ProgramsPractice;
+
+public class EmployeeStream {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String dept;
+	
+	private Double salary;
+
+	public EmployeeStream(Integer id, String name, String dept, Double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dept = dept;
+		this.salary = salary;
+	}
+	
+	
+}
