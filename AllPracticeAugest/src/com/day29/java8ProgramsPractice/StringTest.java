09d82d492491ae2e9bf84e442f8ed502eda37762
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-29 16:28:09 +0530

    java String is immutable which means it cannot be changed string is a sequence of characters

---
+++ AllPracticeAugest/src/com/day29/java8ProgramsPractice/StringTest.java
@@ -0,0 +1,17 @@
+package com.day29.java8ProgramsPractice;
+
+// java String is immutable which means it cannot be changed
+// we change any string, a new instance is created
+// string is a sequence of characters
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		
+		String s1 = "Chandrakant";
+		String s2 = "Chandrakant";
+		
+		System.out.println(s1+ " = " +s2);
+		
+	}
+}
