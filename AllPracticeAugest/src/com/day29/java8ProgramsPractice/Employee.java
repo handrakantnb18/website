415bc087f544b5f678fb71c862218be4f0416094
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-29 10:27:28 +0530

    Employee getter and setter method

--- AllPracticeAugest/src/com/day29/java8ProgramsPractice/Employee.java
+++ AllPracticeAugest/src/com/day29/java8ProgramsPractice/Employee.java
@@ -21,6 +21,74 @@ public class Employee {
 		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", dept=" + dept + ", salary=" + salary
 				+ ", designation=" + designation + ", address=" + address + "]";
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Employee(Integer id, String name, String email, String dept, Double salary, String designation,
+			String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.dept = dept;
+		this.salary = salary;
+		this.designation = designation;
+		this.address = address;
+	}
 	
 	
 	
