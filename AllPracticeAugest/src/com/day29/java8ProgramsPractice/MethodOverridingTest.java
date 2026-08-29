bf2a04aa0011ed105ab438a4db72f3aa9eb90346
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-29 16:23:11 +0530

    Method overriding occurs in two classes that have IS-A (inheritance) relationship. Number and type of parameter must be the same.

---
+++ AllPracticeAugest/src/com/day29/java8ProgramsPractice/MethodOverridingTest.java
@@ -0,0 +1,32 @@
+package com.day29.java8ProgramsPractice;
+
+// Method overriding occurs in two classes that have IS-A (inheritance) relationship.
+//Number and type of parameter must be the same.
+// Return type must be same or covariant.
+
+class Animal {
+	
+	public void makeSound() {
+		System.out.println("Animal is a sound.");
+	}
+}
+
+class Dog extends Animal {
+	
+	@Override
+	public void makeSound() {
+		System.out.println("Dog barks");
+	}
+}
+
+public class MethodOverridingTest {
+
+	public static void main(String[] args) {
+		
+		Animal a = new Animal();
+		Animal d = new Dog();
+		a.makeSound();
+		d.makeSound();
+		
+	}
+}
