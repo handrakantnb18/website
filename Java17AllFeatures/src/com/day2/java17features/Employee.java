a619cccbecf34a33bd90a1abd05c8901c09ed6dd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-30 17:52:26 +0530

    getter and setter method and toString method

--- Java17AllFeatures/src/com/day2/java17features/Employee.java
+++ Java17AllFeatures/src/com/day2/java17features/Employee.java
@@ -27,6 +27,68 @@ public class Employee {
 		this.dept = dept;
 		this.designation = designation;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", salary=" + salary + ", city=" + city
+				+ ", dept=" + dept + ", designation=" + designation + "]";
+	}
 	
 	
 }
