40639d3d55a5a011697473eb1d519ecc9340d014
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-30 17:51:37 +0530

    Employee class private Variables and constructor

---
+++ Java17AllFeatures/src/com/day2/java17features/Employee.java
@@ -0,0 +1,32 @@
+package com.day2.java17features;
+
+public class Employee {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private Double salary;
+	
+	private String city;
+	
+	private String dept;
+	
+	private String designation;
+
+	public Employee(Integer id, String name, String email, Double salary, String city, String dept,
+			String designation) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.salary = salary;
+		this.city = city;
+		this.dept = dept;
+		this.designation = designation;
+	}
+	
+	
+}
