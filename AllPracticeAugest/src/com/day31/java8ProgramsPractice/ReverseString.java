6b01b2e3c93fd887a996f2d24a2cebfea139f858
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-08-31 19:16:05 +0530

    Reverse String into String

---
+++ AllPracticeAugest/src/com/day31/java8ProgramsPractice/ReverseString.java
@@ -0,0 +1,18 @@
+package com.day31.java8ProgramsPractice;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		String str = "chandrakant bhosale";
+		
+		String rev = "";
+		
+		for(int i = str.length() - 1; i >= 0; i--)
+		{
+			rev += str.charAt(i);
+		}
+		
+		System.out.println(rev);
+	}
+}
