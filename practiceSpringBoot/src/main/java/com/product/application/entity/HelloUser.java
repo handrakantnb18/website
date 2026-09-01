2c4b8e97724b645d385a50e6b0baa9155956f4bb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-01 21:43:56 +0530

    HelloUser class private variables and getter and setter method

---
+++ practiceSpringBoot/src/main/java/com/product/application/entity/HelloUser.java
@@ -0,0 +1,65 @@
+package com.product.application.entity;
+
+import java.sql.Date;
+
+public class HelloUser {
+
+	private Integer id;
+	
+	private String name;
+	
+	private Date dob;
+	
+	
+	
+	public HelloUser(Integer id, String name, Date dob) {
+		this.id = id;
+		this.name = name;
+		this.dob =dob;
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public Date getDob() {
+		return dob;
+	}
+
+
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "HelloUser [id=" + id + ", name=" + name + ", dob=" + dob + "]";
+	}
+	
+	
+}
