82d6e52a3079dd5f7f73968603d0936eb2d605c2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-01 22:09:14 +0530

    HelloWorldController.java file and add another helloWorldBean() service.

--- practiceSpringBoot/src/main/java/com/product/application/controller/HelloController.java
+++ practiceSpringBoot/src/main/java/com/product/application/controller/HelloController.java
@@ -4,14 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class HelloController {
 
-	
-	@GetMapping(path="hello-word")
+	@GetMapping(path = "hello-word")
 	public String helloWord() {
 		return "Hello Word";
 	}
-	
-	@GetMapping(path="hello-word")
-	public String helloWord1() {
-		return "Hello Word";
+
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 }
