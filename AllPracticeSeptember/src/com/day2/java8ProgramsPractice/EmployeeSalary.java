1beda589ca0fd57c43fab0a4b6c394215fcd2889
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-02 13:44:05 +0530

    Employee Salary get salary greater than value

--- AllPracticeSeptember/src/com/day2/java8ProgramsPractice/EmployeeSalary.java
+++ AllPracticeSeptember/src/com/day2/java8ProgramsPractice/EmployeeSalary.java
@@ -14,10 +14,10 @@ public class EmployeeSalary {
 
 	Map<Integer, Employee> emp = new HashMap<Integer, Employee>();
 
-	 emp.put(101, new Employee(101, "Rahul", "rahul@gmial.com", 78000.00, "IT")),
-	 emp.put(102, new Employee(102, "Amit", "amit@gmial.com", 80000.00, "IT")),
-	 emp.put(103, new Employee(103, "Vijat", "vijay@gmial.com", 98000.00, "IT")),
-	 emp.put(104, new Employee(104, "pooja", "pooja@gmial.com", 55000.00, "IT"));
+//	 emp.put(101, new Employee(101, "Rahul", "rahul@gmial.com", 78000.00, "IT")),
+//	 emp.put(102, new Employee(102, "Amit", "amit@gmial.com", 80000.00, "IT")),
+//	 emp.put(103, new Employee(103, "Vijat", "vijay@gmial.com", 98000.00, "IT")),
+//	 emp.put(104, new Employee(104, "pooja", "pooja@gmial.com", 55000.00, "IT"));
 
 //		List<Employee> employee = Arrays.asList(
 //				new Employee(101, "Ram", "ram@gmail.com", 75000.00, "IT"),
@@ -32,12 +32,12 @@ public class EmployeeSalary {
 //			System.out.println(id);
 //		});
 		
-		emp.values()
-		.stream()
-		.count(Comparator.comparing(Employee::getSalary > 75000.00))
-		.max()
-		.forEach(System.out::println);
-		
-		System.out.println(emp1);
+//		emp.values()
+//		.stream()
+//		.count(Comparator.comparing(Employee::getSalary > 75000.00))
+//		.max()
+//		.forEach(System.out::println);
+//		
+//		System.out.println(emp1);
 	}
 }
