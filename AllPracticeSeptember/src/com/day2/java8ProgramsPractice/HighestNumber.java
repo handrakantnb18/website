de42cc9b269c3270965e8eebf238b54abb29138d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-02 19:42:30 +0530

    Find the highest number

---
+++ AllPracticeSeptember/src/com/day2/java8ProgramsPractice/HighestNumber.java
@@ -0,0 +1,23 @@
+package com.day2.java8ProgramsPractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+// Find the highest number
+public class HighestNumber {
+
+	public static void main(String[] args) {
+		
+		List<Integer> num = Arrays.asList(
+				10, 20, 5, 30, 40, 40);
+		
+		int max =
+		num.stream()
+		.max(Integer::compareTo)
+		.orElseThrow();
+		
+		System.out.println(max);
+		
+		
+	}
+}
