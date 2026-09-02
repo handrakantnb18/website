1a4f2f71fdc3becbe71c9cbe7f85b383f4568457
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-02 20:01:14 +0530

    Handling Arithmetic Exception (Divide by Zero)

---
+++ AllPracticeSeptember/src/com/day3/java8ProgramsPractice/TryCatchTestThree.java
@@ -0,0 +1,17 @@
+package com.day3.java8ProgramsPractice;
+
+public class TryCatchTestThree {
+
+	public static void main(String[] args) {
+		
+		try  {    
+	        int data=50/0; //may throw exception     
+	        }    
+	        //handling the exception    
+	        catch(ArithmeticException e)   {    
+	            System.out.println(e);    
+	        }    
+	        System.out.println("rest of the code");    
+	        
+	}
+}
