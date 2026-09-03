02b99ca8d0a99a79ac84a03a48aced2118e411fb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-03 22:12:43 +0530

    getter and setter and method and constructor

--- Java17AllFeatures/src/com/day3/java17features/Employee.java
+++ Java17AllFeatures/src/com/day3/java17features/Employee.java
@@ -17,6 +17,55 @@ public class Employee {
 		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", dept=" + dept + ", salary=" + salary
 				+ "]";
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public Employee(Integer id, String name, String email, String dept, Double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.dept = dept;
+		this.salary = salary;
+	}
 	
 	
 }
