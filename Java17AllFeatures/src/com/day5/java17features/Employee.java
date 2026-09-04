fad784834689996b677667fdd7f1ed584788df1a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-04 23:45:54 +0530

    getter and setter method and constructor

--- Java17AllFeatures/src/com/day5/java17features/Employee.java
+++ Java17AllFeatures/src/com/day5/java17features/Employee.java
@@ -19,6 +19,69 @@ private Integer id;
 		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", salary=" + salary + ", dept=" + dept
 				+ ", city=" + city + "]";
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Employee(Integer id, String name, String email, Double salary, String dept, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.salary = salary;
+		this.dept = dept;
+		this.city = city;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	
 	
 }
