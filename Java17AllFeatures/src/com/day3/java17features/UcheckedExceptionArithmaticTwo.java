0ebdc9a5705583af3bd7439dce5b646e2a6e9ee9
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-04 23:43:13 +0530

    Every subclass of Error and RuntimeException is an unchecked exception in Java. A checked exception is everything else under the Throwable class.

---
+++ Java17AllFeatures/src/com/day3/java17features/UcheckedExceptionArithmaticTwo.java
@@ -0,0 +1,26 @@
+package com.day3.java17features;
+
+import java.io.*;
+
+// Every subclass of Error and RuntimeException is an unchecked exception in
+// Java. A checked exception is everything else under the Throwable class.
+public class UcheckedExceptionArithmaticTwo {
+
+	public static void method() throws FileNotFoundException {
+		FileReader file = new FileReader("C:\\Users\\Desktop\\abc.txt");
+		BufferedReader fileInput = new BufferedReader(file);
+		throw new FileNotFoundException();
+	}
+
+	public static void main(String[] args) {
+
+		 try  {    
+	            method();    
+	        }     
+	        catch (FileNotFoundException e)     {    
+	            e.printStackTrace();    
+	        }    
+	        System.out.println("rest of the code...");      
+	          
+	}
+}
