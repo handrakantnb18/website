cfa1b9ed4ced2be3f08e3cf5a040b7b4da3e3506
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-05 20:54:43 +0530

    how to sort string elements in descending order using Collections.sort() with Collections.reverseOrder():

---
+++ Java17AllFeatures/src/com/day5/java17features/CollectionSortTestTwo.java
@@ -0,0 +1,26 @@
+package com.day5.java17features;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class CollectionSortTestTwo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("1. Ram");
+		list.add("2. Saurav");  
+		list.add("3. Mukesh");  
+		list.add("4. Tahir");
+		list.add("5. Vijay");
+		
+		Collections.sort(list, Collections.reverseOrder());
+		Iterator i = list.iterator();
+		
+		while(i.hasNext())
+		{
+			System.out.println(i.next());
+		}
+	}
+}
