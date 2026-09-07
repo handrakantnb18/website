51badf1d766acdad7e51e073a8fa0bc34f483a0d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-07 17:05:15 +0530

    Sorting Map by Key (Descending Order)

---
+++ AllPracticeSeptember/src/com/day5/java8ProgramsPractice/MapInterfaceTestThree.java
@@ -0,0 +1,22 @@
+package com.day5.java8ProgramsPractice;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapInterfaceTestThree {
+
+	public static void main(String[] args) {
+		
+		Map<Integer, String> map = new HashMap<Integer, String>();
+		 map.put(100, "Amit");  
+	        map.put(101, "Vijay");  
+	        map.put(102, "Rahul");  
+	        
+	        map.entrySet()
+	        .stream()
+	        .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
+	        .forEach(System.out::println);
+	        
+	}
+}
