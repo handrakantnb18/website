a5893f00371d0e932f4064dc9a2c53775b3c2421
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-07 16:58:44 +0530

    we are using a non-generic Map and traversing it using Iterator.

---
+++ AllPracticeSeptember/src/com/day7/java8ProgramsPractice/MapInterfaceTest.java
@@ -0,0 +1,28 @@
+package com.day7.java8ProgramsPractice;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class MapInterfaceTest {
+
+	public static void main(String[] args) {
+		
+		Map map = new HashMap();
+		map.put(1,"Amit");  
+	    map.put(5,"Rahul");  
+	    map.put(2,"Jai");  
+	    map.put(6,"Amit");  
+	    
+	    Set set=map.entrySet();
+	    
+	    Iterator itr=set.iterator();  
+	    while(itr.hasNext()){  
+	    	
+	    	Map.Entry entry=(Map.Entry)itr.next();  
+	        System.out.println(entry.getKey()+" "+entry.getValue());
+	        
+	    }
+	}
+}
