aa2dcf700b414013ad01d57b5c70c17dd26a1280
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-07 15:37:44 +0530

    InvalidAgeException custom exception has been created

---
+++ AllPracticeSeptember/src/com/day6/java8ProgramsPractice/InvalidAgeException.java
@@ -0,0 +1,9 @@
+package com.day6.java8ProgramsPractice;
+
+public class InvalidAgeException extends RuntimeException {
+
+	public InvalidAgeException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+}
