541ef16abdf8ad06647141077960bc026046e446
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-08 22:05:40 +0530

    method overloading by changing the data type of arguments in Java.

---
+++ AllPracticeSeptember/src/com/day7/java8ProgramsPractice/OverloadingTest.java
@@ -0,0 +1,24 @@
+package com.day7.java8ProgramsPractice;
+
+class Adder {
+	static int add(int a, int b) {
+		return a + b;
+
+	}
+
+	static double add(double a, double b) {
+		return a + b;
+
+	}
+}
+
+public class OverloadingTest {
+
+	public static void main(String[] args) {
+
+		System.out.println(Adder.add(11, 11));
+
+		System.out.println(Adder.add(12.3, 12.6));
+
+	}
+}
