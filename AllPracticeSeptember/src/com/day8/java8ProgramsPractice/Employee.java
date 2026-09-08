7cc8e341e9642ec0cdafaac71c19b064d26a37dd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-08 10:22:40 +0530

    Employee class getter and setter method created

--- AllPracticeSeptember/src/com/day8/java8ProgramsPractice/Employee.java
+++ AllPracticeSeptember/src/com/day8/java8ProgramsPractice/Employee.java
@@ -19,6 +19,65 @@ public class Employee {
 		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", salary=" + salary + ", city=" + city
 				+ ", dept=" + dept + "]";
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public Employee(Integer id, String name, String email, Double salary, String city, String dept) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.salary = salary;
+		this.city = city;
+		this.dept = dept;
+	}
+	
 	
 	
 }
