cfd4ef7e152d9905a834b5e150f1cdcfc3c262c0
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-10 03:14:33 +0530

    How was my Walk-in Drive experience at Capgemini

--- AllPracticeSeptember/src/com/day1/java8ProgramsPractice/Employee.java
+++ AllPracticeSeptember/src/com/day1/java8ProgramsPractice/Employee.java
@@ -80,3 +80,38 @@ public class Employee {
 	
 	
 }
+
+/*
+ * How was my Walk-in Drive experience at Capgemini?
+ * 
+ * Total Sections - L1,L2 and HR Time for each sections = 30 to 40 minutes Mode
+ * - F2F( Face to Face) Drive Date - 5th Sept 2026 Role - Java Backend/ Java
+ * full stack( Angular/React) Exp - 4 to 10 years
+ * 
+ * L1 Round -
+ * 
+ * 1. Tell me about your self 2. What is Springboot vs microservices difference?
+ * 3. When will you use microservices and when will you monolithic services
+ * based architecture? 4. What is @component, @service and @repository
+ * annotation does and what happens behind? 5. Where are beans stored once they
+ * are invoked? 6. What is Spring IOC Container and how it stores the beans? 7.
+ * Write a normal REST API which shows @controller, @service and @repository
+ * whole flow? 8.What is component scan? Let's say I don't want to include any
+ * class as spring oriented so how to exclude it ? 9. What is Null pointer
+ * Exception? Have you ever faced in your project? 10. What is ArrayoutofBound
+ * Exception? 11. Why is String Immutable in nature? Explain Heap Memory and
+ * String pool? 12. Why do we use Optional and What are methods to prevent Null
+ * pointer exception?
+ * 
+ * L2 Round -
+ * 
+ * 1. Tell me about your project and roles and responsibilities? 2. Have you
+ * used java 21 in your projects? 3. How much are you comfortable in Java8? 4.
+ * What is functional Interface? Difference between Abstract vs Static methods.
+ * 5. What is difference between Consumer vs Predicate? 6. Difference between
+ * ArrayList vs LinkedList? 7. What is Has-a relationship vs Is-a-relationship?
+ * 8. Why String is Immutable with example? 9. Difference between ArrayList vs
+ * Linkedinlist. 10. What are Marker Interfaces? What are built-in methods? 11.
+ * Write a Dynamic Forms in Angular using Template driven or Reactive Forms.
+ * 
+ */
