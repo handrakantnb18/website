1281241e62a340fa653ee20a0a25f4f5ad755c17
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-09 17:24:12 +0530

    ArraysList Test Threen

---
+++ AllPracticeSeptember/src/com/day9/java8ProgramsPractice/ArraysListTestThreen.java
@@ -0,0 +1,31 @@
+package com.day9.java8ProgramsPractice;
+
+import java.util.ArrayList;
+
+public class ArraysListTestThreen {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<>();  
+		  
+        list.add("1. Ravi");  
+        list.add("2. Vijay");  
+        list.add("3. Ajay"); 
+        list.add("4. Ram");  
+        list.add("5. Vaibhav");  
+        list.add("6. Pooja");
+        list.add("7. Vaibhav");  
+        list.add("8. Pooja");
+  
+        list.add(4  , "7. Gaurav");  
+  
+        ArrayList<String> list2 = new ArrayList<>();  
+        list2.add("8. Sonoo");  
+        list2.add("9. Hanumat");  
+  
+        list.addAll(list2);  
+  
+        System.out.println(list);  
+
+	}
+}
