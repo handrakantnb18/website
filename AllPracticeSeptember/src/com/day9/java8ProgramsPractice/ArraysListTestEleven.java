d82e9f6298a03093b3fd3ac4da6e2e5d98f254e2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-09 12:40:36 +0530

    demonstrates how to check if an ArrayList is empty.

---
+++ AllPracticeSeptember/src/com/day9/java8ProgramsPractice/ArraysListTestEleven.java
@@ -0,0 +1,17 @@
+package com.day9.java8ProgramsPractice;
+
+import java.util.ArrayList;
+
+public class ArraysListTestEleven {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<>();  
+		  
+        System.out.println(list.isEmpty());  
+  
+        list.add("Ravi");  
+  
+        System.out.println(list.isEmpty()); 
+	}
+}
