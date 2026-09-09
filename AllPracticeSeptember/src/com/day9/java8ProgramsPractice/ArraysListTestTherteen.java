bc526835f30e89c8efbac09fc89373e9d5732167
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-09 12:45:57 +0530

    he difference between size and capacity of an ArrayList.

---
+++ AllPracticeSeptember/src/com/day9/java8ProgramsPractice/ArraysListTestTherteen.java
@@ -0,0 +1,14 @@
+package com.day9.java8ProgramsPractice;
+
+import java.util.ArrayList;
+
+public class ArraysListTestTherteen {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> list = new ArrayList<>(10);
+
+		System.out.println("Size: " + list.size());
+
+	}
+}
