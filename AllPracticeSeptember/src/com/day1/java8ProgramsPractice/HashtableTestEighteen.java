6c5f644616d4260f531fddac04c4b158af2572fe
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-11 10:17:38 +0530

    how to use the getOrDefault() method

---
+++ AllPracticeSeptember/src/com/day1/java8ProgramsPractice/HashtableTestEighteen.java
@@ -0,0 +1,21 @@
+package com.day1.java8ProgramsPractice;
+
+import java.util.Hashtable;
+
+// how to use the getOrDefault() method
+
+public class HashtableTestEighteen {
+
+	public static void main(String[] args) {
+		
+		Hashtable<Integer,String> map=new Hashtable<Integer,String>();          
+	     map.put(100,"Amit");    
+	     map.put(102,"Ravi");   
+	     map.put(101,"Vijay");    
+	     map.put(103,"Rahul");    
+	       
+	     System.out.println(map.getOrDefault(101, "Not Found"));  
+	     System.out.println(map.getOrDefault(105, "Not Found"));  
+	     
+	}
+}
