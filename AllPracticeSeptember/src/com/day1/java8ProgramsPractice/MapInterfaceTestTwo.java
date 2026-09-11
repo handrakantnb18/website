79d75d2e9f05ea46219dbee227af92424f232550
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-11 10:17:07 +0530

    we are using a generic Map and traversing it using entrySet().

---
+++ AllPracticeSeptember/src/com/day1/java8ProgramsPractice/MapInterfaceTestTwo.java
@@ -0,0 +1,22 @@
+package com.day1.java8ProgramsPractice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// we are using a generic Map and traversing it using entrySet().
+
+public class MapInterfaceTestTwo {
+
+	public static void main(String[] args) {
+		
+		Map<Integer, String> map = new HashMap<Integer, String>();
+		map.put(100, "Amit");
+		map.put(101, "Vijay");
+		map.put(102, "Rahul");
+
+		for (Map.Entry m : map.entrySet()) {
+			System.out.println(m.getKey() + " " + m.getValue());
+		}
+		
+	}
+}
