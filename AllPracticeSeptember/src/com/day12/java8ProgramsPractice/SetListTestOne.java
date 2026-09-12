d508f619745bb1aa696d10e638f30b349d0c8646
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-12 20:41:19 +0530

    it does not allow duplicate values. The set interface provides efficient search, insertion, and deletion operations.

---
+++ AllPracticeSeptember/src/com/day12/java8ProgramsPractice/SetListTestOne.java
@@ -0,0 +1,18 @@
+package com.day12.java8ProgramsPractice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// it does not allow duplicate values.
+// The set interface provides efficient search, insertion, and deletion operations.
+
+public class SetListTestOne {
+
+	public static void main(String[] args) {
+		
+		Set<String> str = new HashSet<String>();
+		
+		System.out.println("Set Emenets : "+str);
+		
+	}
+}
