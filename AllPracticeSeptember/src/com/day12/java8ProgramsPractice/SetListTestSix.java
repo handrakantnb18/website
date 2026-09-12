9330c6c047768c954b9a63aeba23257554cd2bcb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-12 20:43:31 +0530

    SetListTestSix

---
+++ AllPracticeSeptember/src/com/day12/java8ProgramsPractice/SetListTestSix.java
@@ -0,0 +1,24 @@
+package com.day12.java8ProgramsPractice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SetListTestSix {
+
+	public static void main(String[] args) {
+		
+		Map<Integer, String> map = new HashMap<Integer, String>();
+		map.put(1, "Ram");
+        map.put(2, "Rahul");
+        map.put(3, "Rani");
+        map.put(4, "Ragini");
+        map.put(5, "Ravi");
+        map.put(6, "Raju");
+        map.put(7, "Rajat");
+        map.put(8, "Ramesh");
+        map.put(9, "Ranjeet");
+        
+        System.out.println(map);
+        
+	}
+}
