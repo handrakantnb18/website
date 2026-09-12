30e1858bec35a2d8b1cb174a288606b7f9c35957
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-12 20:55:20 +0530

    SubscriptionService 	private final SubscriptionRepository repository;

---
+++ AllPracticeSeptember/src/com/day13/java8ProgramsPractice/SubscriptionService.java
@@ -0,0 +1,32 @@
+package com.day13.java8ProgramsPractice;
+
+import java.time.Instant;
+
+//@Service
+public class SubscriptionService {
+
+//	private final SubscriptionRepository repository;
+//	
+//	public SubscriptionService(SubscriptionRepository) {
+//		this.repository = repository;
+//	}
+//	
+//	public Subscription createSubscription(Long user) {
+//		
+//		Subscription existing = repository.findByUserId(userId);
+//		
+//		if(existing != null) {
+//			throw new RuntimeException("User already has a subscription");
+//		}
+//		
+//		Subscription subscription = new Subscription();
+//		
+//		subscription.setUserID(userId);
+//		subscription.setPlanId(plainId);
+//		subscription.setStartDate(SubscriptionStatus.A);
+//		subscription.setStartDate(Instant.now());
+//		
+//		return repository.save(subscription);
+		
+//	}
+}
