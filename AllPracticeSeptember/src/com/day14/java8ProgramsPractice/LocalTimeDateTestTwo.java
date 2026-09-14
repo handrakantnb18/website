6998f198e71dc28ad8cd4ccecebcb5fff59012b3
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-14 12:24:51 +0530

    import the java.time.LocalDateTime class, and use its now() method:

---
+++ AllPracticeSeptember/src/com/day14/java8ProgramsPractice/LocalTimeDateTestTwo.java
@@ -0,0 +1,16 @@
+package com.day14.java8ProgramsPractice;
+
+import java.time.LocalDateTime;
+
+//  import the java.time.LocalDateTime class, 
+// and use its now() method:
+
+public class LocalTimeDateTestTwo {
+
+	public static void main(String[] args) {
+		
+		LocalDateTime l = LocalDateTime.now();
+		System.out.println(l);
+		
+	}
+}
