0a66d14ba76183cc9693ea5cef8fe6667e905382
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-14 11:00:59 +0530

    Employee class private variables toString method

---
+++ AllPracticeSeptember/src/com/day14/java8ProgramsPractice/Employee.java
@@ -0,0 +1,26 @@
+package com.day14.java8ProgramsPractice;
+
+public class Employee {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private Double salary;
+	
+	private String dept;
+	
+	private String city;
+
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", salary=" + salary + ", dept=" + dept
+				+ ", city=" + city + "]";
+	}
+	
+	
+	
+}
