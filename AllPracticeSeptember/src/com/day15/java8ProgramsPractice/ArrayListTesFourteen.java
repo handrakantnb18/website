130334324669d606dc9304db5719f14cba4a33ff
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-15 22:38:44 +0530

    to traverse LinkedList elements.

---
+++ AllPracticeSeptember/src/com/day15/java8ProgramsPractice/ArrayListTesFourteen.java
@@ -0,0 +1,24 @@
+package com.day15.java8ProgramsPractice;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+// to traverse LinkedList elements.
+
+public class ArrayListTesFourteen {
+
+	public static void main(String[] args) {
+
+		LinkedList<String> al = new LinkedList<String>();
+		al.add("Ravi");
+		al.add("Vijay");
+		al.add("Ravi");
+		al.add("Ajay");
+
+		Iterator<String> itr = al.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
+	}
+}
