7b3a76c8203a8499ce25df16f9444238610c1ff1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-15 13:10:39 +0530

    An ArrayList is a resizable array that can grow as needed

---
+++ AllPracticeSeptember/src/com/day15/java8ProgramsPractice/ListTestOne.java
@@ -0,0 +1,20 @@
+package com.day15.java8ProgramsPractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// An ArrayList is a resizable array that can grow as needed
+
+public class ListTestOne {
+
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		list.add("Priyankaa");
+		list.add("Chandrakaant");
+		list.add("Shivraaj");
+		list.add("Bhosaale");
+		
+		System.out.println(list);
+	}
+}
