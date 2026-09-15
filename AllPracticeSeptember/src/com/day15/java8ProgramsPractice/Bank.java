84ff88ef6b53598cab84c283fb856ec211df3c8f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-15 13:10:03 +0530

    toString method and private variables

---
+++ AllPracticeSeptember/src/com/day15/java8ProgramsPractice/Bank.java
@@ -0,0 +1,26 @@
+package com.day15.java8ProgramsPractice;
+
+public class Bank {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	private Double salary;
+	
+	private String dept;
+	
+	private String city;
+
+	
+	@Override
+	public String toString() {
+		return "Bank [id=" + id + ", name=" + name + ", email=" + email + ", salary=" + salary + ", dept=" + dept
+				+ ", city=" + city + "]";
+	}
+	
+	
+	
+}
