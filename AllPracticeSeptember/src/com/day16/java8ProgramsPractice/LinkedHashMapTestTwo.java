af405ead0fb9c5c056987ebdbb1302dd43090b7d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-16 22:35:14 +0530

    we are creating a LinkedHashMap, adding elements, and traversing it.

---
+++ AllPracticeSeptember/src/com/day16/java8ProgramsPractice/LinkedHashMapTestTwo.java
@@ -0,0 +1,26 @@
+package com.day16.java8ProgramsPractice;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+// we are creating a LinkedHashMap, adding elements, and traversing it.
+
+public class LinkedHashMapTestTwo {
+
+	public static void main(String[] args) {
+
+		LinkedHashMap<Integer, String> hm = new LinkedHashMap<Integer, String>();
+
+		hm.put(104, "Ram");
+		hm.put(105, "Chandrakant");
+		hm.put(106, "Pooja");
+		hm.put(100, "Amit");
+		hm.put(101, "Vijay");
+		hm.put(102, "Rahul");
+
+		for (Map.Entry<Integer, String> m : hm.entrySet()) {
+			System.out.println(m.getKey() + " " + m.getValue());
+		}
+
+	}
+}
