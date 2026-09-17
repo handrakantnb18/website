c97e835bdffb90c6331be6cce0231fe3cd6fb2cd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-17 21:08:04 +0530

    Iterating over List Interface in Java Basic for loop with get(index) Enhanced for-each loop

---
+++ AllPracticeSeptember/src/com/day17/java8ProgramsPractice/ArrayListTestSeven.java
@@ -0,0 +1,29 @@
+package com.day17.java8ProgramsPractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Iterating over List Interface in Java
+// Basic for loop with get(index) Enhanced for-each loop
+
+public class ArrayListTestSeven {
+
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		list.add("1. Java");
+		list.add("2. Python");
+		list.add("3. DSA");
+		list.add(1, "4. C++");
+		
+		for(int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i)+" ");
+		}
+		
+		System.out.println();
+		
+		for (String str : list)
+			System.out.println(str+" ");
+		
+	}
+}
