92da3549bbd212bc3a02bf794d8c8589167e3420
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-17 21:15:23 +0530

    Set interface is a part of the Java Collection Framework, located in the java.util package.

---
+++ AllPracticeSeptember/src/com/day17/java8ProgramsPractice/HashSetTestOne.java
@@ -0,0 +1,18 @@
+package com.day17.java8ProgramsPractice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// Set interface is a part of the Java Collection 
+// Framework, located in the java.util package.
+
+public class HashSetTestOne {
+
+	public static void main(String[] args) {
+		
+		Set<String> str = new HashSet<>();
+		
+		System.out.println("Set Elements : "+str);
+		
+	}
+}
