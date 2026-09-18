05ecf0ecc4a2af5a0914d1d547cf8f3b2a7f5cae
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-18 18:52:41 +0530

    Using Temporary Array - O(n) Time and O(n) Space

---
+++ AllPracticeSeptember/src/com/day18/java8ProgramsPractice/RotateCounterTestTwo.java
@@ -0,0 +1,36 @@
+package com.day18.java8ProgramsPractice;
+
+// Using Temporary Array - O(n) Time and O(n) Space
+
+public class RotateCounterTestTwo {
+
+	static void rotateArr(int[] arr, int d) {
+		int n = arr.length;
+
+		d %= n;
+
+		int[] temp = new int[n];
+
+		for (int i = 0; i < n - d; i++)
+			temp[i] = arr[d + i];
+
+		for (int i = 0; i < d; i++)
+			temp[n - d + i] = arr[i];
+
+		for (int i = 0; i < n; i++)
+			arr[i] = temp[i];
+
+	}
+
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 2, 3, 4, 5, 6 };
+		int d = 2;
+
+		rotateArr(arr, d);
+
+		for (int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
+
+	}
+}
