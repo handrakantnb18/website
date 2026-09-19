95c9891f6d2f332c80c8d9aeda5a056a1c4539e7
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-19 11:34:22 +0530

    Using Character Array : Use character array to reverse a string

---
+++ AllPracticeSeptember/src/com/day19/java8ProgramsPractice/ReverseStringTestThree.java
@@ -0,0 +1,17 @@
+package com.day19.java8ProgramsPractice;
+
+// Using Character Array : Use character array to reverse a string. 
+
+public class ReverseStringTestThree {
+
+	public static void main(String[] args) {
+		
+		String str = "chandrakant";
+		
+		char[] arr = str.toCharArray();
+		
+		for(int i = arr.length - 1; i >= 0; i--)
+			System.out.print(arr[i]);
+		
+	}
+}
