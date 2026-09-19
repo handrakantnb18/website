5cf68d80bba54c3b55aa3f683a3879ac108bfde4
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-19 15:02:35 +0530

    Program implementing Bubble Sort in Java

---
+++ AllPracticeSeptember/src/com/day19/java8ProgramsPractice/BubbleSort.java
@@ -0,0 +1,34 @@
+package com.day19.java8ProgramsPractice;
+
+//  Program implementing Bubble Sort in Java
+
+public class BubbleSort {
+
+	void bubbleSort(int arr[]) {
+		int n = arr.length;
+
+		for (int i = 0; i < n - 1; i++)
+			for (int j = 0; j < n - i - 1; j++)
+				if (arr[j] > arr[j + 1]) {
+
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+	}
+
+	public static void main(String[] args) {
+
+		BubbleSort ob = new BubbleSort();
+		int a[] = { 64, 34, 25, 12 };
+
+		ob.bubbleSort(a);
+
+		int n = a.length;
+
+		for (int i = 0; i < n; ++i)
+			System.out.print(a[i] + " ");
+		System.out.println();
+
+	}
+}
