634a74d5ffc03ca5040cc53a1b5e7a4253c5bf57
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-19 11:26:23 +0530

    We can optimize the space complexity of the above method by using HashMap instead of initializing 256 characters array.

---
+++ AllPracticeSeptember/src/com/day19/java8ProgramsPractice/StringAnagramTestFour.java
@@ -0,0 +1,60 @@
+package com.day19.java8ProgramsPractice;
+
+import java.util.HashMap;
+import java.util.Set;
+
+// We can optimize the space complexity of the above method by
+// using HashMap instead of initializing 256 characters array.
+
+public class StringAnagramTestFour {
+
+	public static boolean areAnagram(String a, String b) {
+
+		if (a.length() != b.length()) {
+			return false;
+		}
+
+		HashMap<Character, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < a.length(); i++) {
+
+			if (map.containsKey(a.charAt(i))) {
+
+				map.put(a.charAt(i), map.get(a.charAt(i)) + 1);
+			} else {
+
+				map.put(a.charAt(i), 1);
+			}
+		}
+
+		for (int i = 0; i < b.length(); i++) {
+
+			if (map.containsKey(b.charAt(i))) {
+
+				map.put(b.charAt(i), map.get(b.charAt(i)) - 1);
+			}
+		}
+
+		Set<Character> keys = map.keySet();
+
+		for (Character key : keys) {
+			if (map.get(key) != 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+
+		String str1 = "geeksforgeeks";
+		String str2 = "forgeeksgeeks";
+
+		if (areAnagram(str1, str2))
+			System.out.print("The two strings are " + "anagram of each other");
+		else
+			System.out.print("The two strings are " + "not anagram of each other");
+
+	}
+}
