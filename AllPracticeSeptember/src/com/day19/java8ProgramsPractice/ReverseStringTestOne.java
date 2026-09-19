6029facea1e1ed92635aa98b226b2d17e8982dcc
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-19 11:33:08 +0530

    The for loop is the most basic and manual approach.

---
+++ AllPracticeSeptember/src/com/day19/java8ProgramsPractice/ReverseStringTestOne.java
@@ -0,0 +1,20 @@
+package com.day19.java8ProgramsPractice;
+
+// The for loop is the most basic and manual approach. 
+
+public class ReverseStringTestOne {
+
+	public static void main(String[] args) {
+		
+		String str = "chandrakant";
+		
+		String rev ="";
+		
+		for(int i = 0; i <str.length(); i++) {
+			rev = str.charAt(i) + rev;
+			
+		}
+		
+		System.out.println(rev);
+	}
+}
