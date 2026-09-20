709b4f92a015394b65c889fe98f1234622f5d23c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-20 21:49:33 +0530

    StringBuilder provides a built-in reverse() method, making string reversal quick and efficient

---
+++ AllPracticeSeptember/src/com/day20/java8ProgramsPractice/ReverseStringTestTwo.java
@@ -0,0 +1,21 @@
+package com.day20.java8ProgramsPractice;
+
+//StringBuilder provides a built-in reverse() 
+//method, making string reversal quick and efficient
+
+public class ReverseStringTestTwo {
+
+	public static void main(String[] args) {
+
+		String str = "chandrakant";
+
+		StringBuilder res = new StringBuilder();
+
+		res.append(str);
+
+		res.reverse();
+
+		System.out.println(res);
+
+	}
+}
