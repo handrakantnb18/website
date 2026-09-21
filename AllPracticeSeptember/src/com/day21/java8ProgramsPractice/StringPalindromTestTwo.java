02323032223e24fd2e00b052c540f271a83fc817
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-21 20:54:47 +0530

    we use a two-pointer approach similar to iteration.

---
+++ AllPracticeSeptember/src/com/day21/java8ProgramsPractice/StringPalindromTestTwo.java
@@ -0,0 +1,26 @@
+package com.day21.java8ProgramsPractice;
+
+//we use a two-pointer approach similar to iteration.
+
+public class StringPalindromTestTwo {
+
+	public static boolean isPalindrome(String s, int i, int j) {
+
+		if (i >= j)
+			return true;
+
+		if (s.charAt(i) != s.charAt(j))
+			return false;
+
+		return isPalindrome(s, i + 1, j - 1);
+	}
+
+	public static void main(String[] args) {
+
+		String s = "Racecar";
+		s = s.toLowerCase();
+
+		System.out.println(isPalindrome(s, 0, s.length() - 1));
+
+	}
+}
