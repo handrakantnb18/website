52bef083316c206a29f84ce9e689a179fce4868b
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-21 21:27:48 +0530

    Recursive Method for Binary Search

---
+++ AllPracticeSeptember/src/com/day21/java8ProgramsPractice/BinarySearchTestTwo.java
@@ -0,0 +1,41 @@
+package com.day21.java8ProgramsPractice;
+
+//Recursive Method for Binary Search
+
+public class BinarySearchTestTwo {
+
+	static int binarySearch(int a[], int l, int r, int x) {
+
+		if (r >= l) {
+			int m = l + (r - l) / 2;
+
+			if (a[m] == x)
+				return m;
+
+			if (a[m] > x)
+				return binarySearch(a, l, m - 1, x);
+
+			return binarySearch(a, m + 1, r, x);
+		}
+
+		return -1;
+	}
+
+	public static void main(String[] args) {
+
+		int a[] = { 2, 3, 4, 10, 40 };
+		int n = a.length;
+		int x = 10;
+
+		int res = binarySearch(a, 0, n - 1, x);
+
+		System.out.println("Element to be searched is : " + x);
+
+		if (res == -1)
+			System.out.println("Element is not present in array");
+		else
+			System.out.println("Element is present at index: " + res);
+
+	}
+
+}
