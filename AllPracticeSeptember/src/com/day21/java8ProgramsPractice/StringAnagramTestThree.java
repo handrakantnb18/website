ede051a13f28461f1d906fb3433fb59dc6fa0feb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-21 21:00:54 +0530

    The above implementation can be further to use only one count array instead of tw

---
+++ AllPracticeSeptember/src/com/day21/java8ProgramsPractice/StringAnagramTestThree.java
@@ -0,0 +1,42 @@
+package com.day21.java8ProgramsPractice;
+
+//The above implementation can be further to use only one count array instead of tw
+
+public class StringAnagramTestThree {
+
+	static int NO_OF_CHARS = 256;
+
+	static boolean areAnagram(char[] str1, char[] str2) {
+
+		int[] count = new int[NO_OF_CHARS];
+		int i;
+
+		for (i = 0; i < str1.length; i++) {
+			count[str1[i] - 'a']++;
+			count[str2[i] - 'a']--;
+		}
+
+		if (str1.length != str2.length)
+			return false;
+
+		for (i = 0; i < NO_OF_CHARS; i++)
+			if (count[i] != 0) {
+				return false;
+			}
+		return true;
+
+	}
+
+	public static void main(String[] args) {
+
+		char str1[] = "chandrakantbho".toCharArray();
+		char str2[] = "priyankabhosale".toCharArray();
+
+		if (areAnagram(str1, str2))
+			System.out.print("The two strings are " + "anagram of each other");
+		else
+			System.out.print("The two strings are " + "not anagram of each other");
+
+	}
+
+}
