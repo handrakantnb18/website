f121e04b8720dd06ff0af5d42196dc8aeb7ce069
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-21 21:19:54 +0530

    duplicate add list name the fetch ConcurrentModificationException

---
+++ AllPracticeSeptember/src/com/day21/java8ProgramsPractice/FailFastTest.java
@@ -0,0 +1,30 @@
+package com.day21.java8ProgramsPractice;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+//duplicate add list name the fetch ConcurrentModificationException
+
+public class FailFastTest {
+
+	public static void main(String[] args) {
+
+		List<String> list = new ArrayList<String>();
+		list.add("Apple");
+		list.add("Banana");
+		list.add("Orange");
+
+		Iterator<String> itr = list.iterator();
+		while (itr.hasNext()) {
+			String fruit = itr.next();
+			System.out.println(fruit);
+
+			// if(fruit.equals("Graps")) // no error founds in this add list
+			if (fruit.equals("Apple")) { // found error
+				list.add("Apple");
+			}
+		}
+	}
+
+}
