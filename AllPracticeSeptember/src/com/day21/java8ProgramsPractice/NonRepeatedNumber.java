66d57f1ad668fde661b31f8c9d0fc5c668032079
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-21 21:24:56 +0530

    Find the first non-repeated character in a String

---
+++ AllPracticeSeptember/src/com/day21/java8ProgramsPractice/NonRepeatedNumber.java
@@ -0,0 +1,20 @@
+package com.day21.java8ProgramsPractice;
+
+public class NonRepeatedNumber {
+
+	// Find the first non-repeated character in a String
+	public static void main(String[] args) {
+
+		String str = "Swiss";
+
+		Character result = str.chars()
+				.mapToObj(c -> (char) c)
+				.filter(c -> str.indexOf(c) == str.lastIndexOf(c))
+				.findFirst()
+				.orElse(null);
+
+		System.out.println(result);
+
+	}
+
+}
