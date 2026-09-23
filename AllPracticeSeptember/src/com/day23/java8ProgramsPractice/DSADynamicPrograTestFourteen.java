5d06a562931dde872cda1f1b78716d8db8e4f9ab
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-23 08:48:52 +0530

    DP is used when problems have overlapping subproblems and optimal substructure

---
+++ AllPracticeSeptember/src/com/day23/java8ProgramsPractice/DSADynamicPrograTestFourteen.java
@@ -0,0 +1,24 @@
+package com.day23.java8ProgramsPractice;
+
+// DP is used when problems have overlapping 
+// subproblems and optimal substructure
+
+public class DSADynamicPrograTestFourteen {
+
+	static int fib(int n, int[] dp) {
+		if (n <= 1)
+			return n;
+		if (dp[n] != -1)
+			return dp[n];
+		return dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
+	}
+
+	public static void main(String[] args) {
+
+		int n = 10;
+		int[] dp = new int[n + 1];
+		java.util.Arrays.fill(dp, -1);
+		System.out.println("Fibonacci(" + n + "): " + fib(n, dp));
+
+	}
+}
