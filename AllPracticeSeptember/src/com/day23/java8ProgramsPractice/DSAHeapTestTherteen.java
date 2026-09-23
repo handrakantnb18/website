1a4af63f50a7cb94ee715fb6a1767185c332b850
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-23 08:48:25 +0530

    A Graph consists of nodes (vertices) connected by edges. It can be represented in Java using an adjacency list

---
+++ AllPracticeSeptember/src/com/day23/java8ProgramsPractice/DSAHeapTestTherteen.java
@@ -0,0 +1,45 @@
+package com.day23.java8ProgramsPractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// A Graph consists of nodes (vertices) connected
+// by edges. It can be represented in Java using an adjacency list
+
+public class DSAHeapTestTherteen {
+
+	public static void addEdge(List<List<Integer>> adj, int i, int j)
+	{
+		adj.get(i).add(j);
+		adj.get(j).add(i);
+	}
+
+	public static void displayAdjList(List<List<Integer>> adj) {
+		for (int i = 0; i < adj.size(); i++) {
+			System.out.print(i + ": ");
+
+			for (int j : adj.get(i)) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int V = 4;
+		List<List<Integer>> adj = new ArrayList<>(V);
+		for (int i = 0; i < V; i++) {
+			adj.add(new ArrayList<>());
+		}
+
+		addEdge(adj, 0, 1);
+		addEdge(adj, 0, 2);
+		addEdge(adj, 1, 2);
+		addEdge(adj, 2, 3);
+
+		System.out.println("Adjacency List Representation:");
+		displayAdjList(adj);
+
+	}
+}
