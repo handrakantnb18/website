bf21be749509ddcbb78c1b2c56287849db49393f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-23 20:42:10 +0530

    Use it when one thread updates the value and other threads only read it

---
+++ AllPracticeSeptember/src/com/day23/java8ProgramsPractice/VolatileMain.java
@@ -0,0 +1,23 @@
+package com.day23.java8ProgramsPractice;
+
+// Use it when one thread updates the value and other threads only read it
+
+public class VolatileMain {
+
+	 private final static int noOfThreads = 2;  
+	  
+	    public static void main(String[] args) throws InterruptedException {  
+	        VolatileData volatileData = new VolatileData();  
+	        Thread[] threads = new Thread[noOfThreads];  
+	  
+	        for (int i = 0; i < noOfThreads; ++i)  
+	            threads[i] = new VolatileThread(volatileData);  
+	  
+	        for (int i = 0; i < noOfThreads; ++i)  
+	            threads[i].start();  
+	  
+	        for (int i = 0; i < noOfThreads; ++i)  
+	            threads[i].join();  
+	    }  
+	    
+}
