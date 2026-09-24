c39ff4f6762f38f1209d2b6f7db4aa37cbd6acf2
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-24 08:08:26 +0530

    Prints a right-angled triangle with numbers in increasing row order, aligned to the right Number Triangle Pattern

---
+++ AllPracticeSeptember/src/com/day24/java8ProgramsPractice/NumberTranglePatternOne.java
@@ -0,0 +1,31 @@
+package com.day24.java8ProgramsPractice;
+
+// Prints a right-angled triangle with numbers in increasing row order, aligned to the right
+// Number Triangle Pattern
+
+public class NumberTranglePatternOne {
+
+	public static void printPattern(int n) {
+		int i, j;
+
+		for (i = 1; i <= n; i++) {
+
+			for (j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+
+			for (j = 1; j <= i; j++) {
+				System.out.print(i + " ");
+			}
+
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int n = 6;
+		printPattern(n);
+
+	}
+}
