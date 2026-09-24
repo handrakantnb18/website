474c720bc5e51ec841d6d5fdfb84f30e549317fc
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-24 08:14:38 +0530

    Number-Changing Pyramid Pattern Prints a pyramid where numbers increase continuously from top to bottom

---
+++ AllPracticeSeptember/src/com/day24/java8ProgramsPractice/NumberTranglePatternFive.java
@@ -0,0 +1,31 @@
+package com.day24.java8ProgramsPractice;
+
+// Number-Changing Pyramid Pattern
+// Prints a pyramid where numbers increase continuously from top to bottom
+
+public class NumberTranglePatternFive {
+
+	public static void printPattern(int n) {
+		int i, j;
+		int num = 1;
+
+		for (i = 1; i <= n; i++) {
+
+			for (j = 1; j <= i; j++) {
+
+				System.out.print(num + " ");
+
+				num++;
+			}
+
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int n = 6;
+		printPattern(n);
+
+	}
+}
