e618d17c5b34e59688948190b7f8027c699fdb9a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-24 08:06:27 +0530

    Use volatile when a variable is accessed by multiple threads

---
+++ AllPracticeSeptember/src/com/day23/java8ProgramsPractice/VolatileThread.java
@@ -0,0 +1,25 @@
+package com.day23.java8ProgramsPractice;
+
+// Use volatile when a variable is accessed by multiple threads
+
+public class VolatileThread extends Thread {
+
+	private final VolatileData data;  
+	  
+    public VolatileThread(VolatileData data) {  
+        this.data = data;  
+    }  
+  
+    public void run() {  
+        int oldValue = data.getCounter();  
+        System.out.println("[Thread " + Thread.currentThread().getId() +  
+                "]: Old value = " + oldValue);  
+  
+        data.increaseCounter();  
+  
+        int newValue = data.getCounter();  
+        System.out.println("[Thread " + Thread.currentThread().getId() +  
+                "]: New value = " + newValue);  
+    }  
+    
+}
