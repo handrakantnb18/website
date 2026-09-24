c756d0608fa368b0fb0845c06eb14beaa0d5b7e5
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-24 08:11:35 +0530

    Prints a pyramid where each row contains numbers from 1 to the row number Number-Increasing Pyramid Pattern

---
+++ AllPracticeSeptember/src/com/day24/java8ProgramsPractice/NumberTranglePatternTwo.java
@@ -0,0 +1,28 @@
+package com.day24.java8ProgramsPractice;
+
+// Prints a pyramid where each row contains numbers from 1 to the row number
+// Number-Increasing Pyramid Pattern 
+
+public class NumberTranglePatternTwo {
+
+	public static void printPattern(int n) {
+		int i, j;
+
+		for (i = 1; i <= n; i++) {
+
+			for (j = 1; j <= i; j++) {
+
+				System.out.print(j + " ");
+			}
+
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int n = 6;
+		printPattern(n);
+
+	}
+}
