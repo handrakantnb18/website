2ecbced328873ab3661de7bb95f75f204a555eb0
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-26 21:53:05 +0530

    Diamond StPrints a diamond shape made of stars.

---
+++ AllPracticeSeptember/src/com/day26/java8ProgramsPractice/NumberTranglePatternTen.java
@@ -0,0 +1,44 @@
+package com.day26.java8ProgramsPractice;
+
+//Diamond Star Pattern
+//Prints a diamond shape made of stars.
+
+public class NumberTranglePatternTen {
+
+	public static void printPattern(int n) {
+		int i, j;
+		int num = 1;
+
+		for (i = 1; i <= n; i++) {
+
+			for (j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+
+			for (j = 1; j <= 2 * i - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		for (i = n - 1; i >= 1; i--) {
+
+			for (j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+
+			for (j = 1; j <= 2 * i - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int n = 6;
+		printPattern(n);
+
+	}
+
+}
