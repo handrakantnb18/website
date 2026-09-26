0f8ab585de92356a939d5d43f09d5bf8bf1bb269
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-09-26 22:10:10 +0530

    reverse pyramid where each row starts from 1 and ends at the row count Number-Increasing Reverse Pyramid Pattern

---
+++ AllPracticeSeptember/src/com/day26/java8ProgramsPractice/NumberTranglePatternThree.java
@@ -0,0 +1,29 @@
+package com.day26.java8ProgramsPractice;
+
+//reverse pyramid where each row starts from 1 and ends at the row count
+//Number-Increasing Reverse Pyramid Pattern
+
+public class NumberTranglePatternThree {
+
+	public static void printPattern(int n) {
+		int i, j;
+
+		for (i = n; i >= 1; i--) {
+
+			for (j = 1; j <= i; j++) {
+
+				System.out.print(j + " ");
+			}
+
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int n = 6;
+		printPattern(n);
+
+	}
+
+}
